e7d5560914b8257a9760e67388e65db7a3e7fa5f
Amey Bhide <abhide@vmware.com>
2015-06-05 12:10:34 -0700

    Add support for port update
    Change-Id: If85a53932784f4e19faa6ac6cd5f3dc9d674331b

--- vmware_nsx/neutron/tests/unit/vmware/nsx_v3_mocks.py
+++ vmware_nsx/neutron/tests/unit/vmware/nsx_v3_mocks.py
@@ -103,3 +103,55 @@ def create_logical_port(lswitch_id, vif_uuid, tags,
         ]
     }
     return FAKE_PORT
+
+
+def get_logical_port(lport_id):
+    FAKE_SWITCH_UUID = uuidutils.generate_uuid()
+    FAKE_PORT_UUID = uuidutils.generate_uuid()
+    FAKE_PORT = {
+        "id": FAKE_PORT_UUID,
+        "display_name": FAKE_NAME,
+        "resource_type": "LogicalPort",
+        "address_bindings": [],
+        "logical_switch_id": FAKE_SWITCH_UUID,
+        "admin_state": "UP",
+        "attachment": {
+            "id": "9ca8d413-f7bf-4276-b4c9-62f42516bdb2",
+            "attachment_type": "VIF"
+        },
+        "switching_profile_ids": [
+            {
+                "value": "64814784-7896-3901-9741-badeff705639",
+                "key": "IpDiscoverySwitchingProfile"
+            },
+            {
+                "value": "fad98876-d7ff-11e4-b9d6-1681e6b88ec1",
+                "key": "SpoofGuardSwitchingProfile"
+            },
+            {
+                "value": "93b4b7e8-f116-415d-a50c-3364611b5d09",
+                "key": "PortMirroringSwitchingProfile"
+            },
+            {
+                "value": "fbc4fb17-83d9-4b53-a286-ccdf04301888",
+                "key": "SwitchSecuritySwitchingProfile"
+            },
+            {
+                "value": "f313290b-eba8-4262-bd93-fab5026e9495",
+                "key": "QosSwitchingProfile"
+            }
+        ]
+    }
+    return FAKE_PORT
+
+
+def update_logical_port(lport_id, existing_payload, name=None,
+                        admin_state=None):
+    if name:
+        existing_payload['display_name'] = name
+    if admin_state is not None:
+        if admin_state:
+            existing_payload['admin_state'] = "UP"
+        else:
+            existing_payload['admin_state'] = "DOWN"
+    return existing_payload
