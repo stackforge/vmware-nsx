cb2053bbc8ab55dd70be5de6367e7c387b1f5129
Roey Chen <roeyc@vmware.com>
2015-06-16 04:36:31 -0700

    Raise NotImplementError for port's admin_state_up updates
    Changing port admin_state_up changes are not supported by the plugin.
    
    Change-Id: Ic1538f7feabb587ea98de5444d786e4af02c6202
    Signed-off-by: Roey Chen <roeyc@vmware.com>

--- vmware_nsx/neutron/plugins/vmware/plugins/nsx_v.py
+++ vmware_nsx/neutron/plugins/vmware/plugins/nsx_v.py
@@ -743,7 +743,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
 
         pnet._raise_if_updates_provider_attributes(net_attrs)
         if net_attrs.get("admin_state_up") is False:
-            raise NotImplementedError(_("admin_state_up=False networks "
+            raise NotImplementedError(_("admin_state_up=False networks
                                         "are not supported."))
 
         # PortSecurity validation checks
@@ -866,6 +866,9 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
                 context, id, port)
             if psec.PORTSECURITY in port['port']:
                 raise NotImplementedError()
+            if 'admin_state_up' in port['port']:
+                raise NotImplementedError(_("Port admin_state_up updates are "
+                                            "not supported."))
             # copy values over - except fixed_ips as
             # they've already been processed
             updates_fixed_ips = port['port'].pop('fixed_ips', [])
