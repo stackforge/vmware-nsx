954ae39680d552e994cbf1c2ac5c9b1d12ce1477
Amey Bhide <abhide@vmware.com>
2015-06-22 13:52:24 -0700

    WIP: Add unit tests for nsxlib-backend methods
    Change-Id: I9ef65ab9e16eeb4a30e7af79636ae3b1e8bd7776

---
+++ vmware_nsx/neutron/tests/unit/vmware/nsxlib/v3/test_switch.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2014 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import mock
+import requests
+
+from oslo_log import log
+
+from vmware_nsx.neutron.plugins.vmware.nsxlib import v3 as nsxlib
+from vmware_nsx.neutron.tests.unit.vmware.nsxlib.v3 import nsxlib_testcase
+from vmware_nsx.neutron.tests.unit.vmware import test_constants_v3
+
+LOG = log.getLogger(__name__)
+
+
+class NsxLibSwitchTestCase(nsxlib_testcase.NsxLibTestCase):
+
+    @mock.patch("vmware_nsx.neutron.plugins.vmware.nsxlib.v3.requests.post")
+    def test_create_logical_switch(self, mock_post):
+        """
+        Test creating a switch returns the correct response and 200 status
+        """
+        # Construct mock response object
+        mock_response = mock.Mock()
+        mock_response.json.return_value = test_constants_v3.FAKE_SWITCH
+        mock_response.status_code = requests.codes.created
+        mock_post.return_value = mock_response
+
+        result = nsxlib.create_logical_switch(
+                    test_constants_v3.FAKE_NAME,
+                    test_constants_v3.FAKE_TZ_UUID)
+        self.assertEqual(test_constants_v3.FAKE_SWITCH, result)
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main(verbosity=2)
