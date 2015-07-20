ac6ef184c3ae2e16b0952d09b359670a043b3a79
Kobi Samoray <ksamoray@vmware.com>
2015-07-20 18:32:00 +0300

    Fix cleanup upon metadata failure
    When metadata proxy creation fails, the cleanup process failed due to the
    following reasons:
    - Ports where still attached to the router
    - Router deletion failed as router_delete checks for metadata config while
      metadata init didn't finish yet
    - Exception reraise forced termination of neutron service. As the plugin
      didn't exit gracefully all its threads, this left a mess in the DB and
      backend.
    
    The patch addresses the issues above.
    
    Change-Id: Ie08478db4ea3ecc11752f2bd8482dd244bd2f509

--- vmware_nsx/neutron/plugins/vmware/plugins/nsx_v_md_proxy.py
+++ vmware_nsx/neutron/plugins/vmware/plugins/nsx_v_md_proxy.py
@@ -151,18 +151,18 @@ class NsxVMetadataProxyHandler:
                 internal_net, internal_subnet = (
                     self._create_metadata_internal_network(INTERNAL_SUBNET))
             except Exception as e:
-                with excutils.save_and_reraise_exception():
-                    nsxv_db.delete_nsxv_internal_network(
-                        self.context.session,
-                        vcns_const.InternalEdgePurposes.INTER_EDGE_PURPOSE)
+                nsxv_db.delete_nsxv_internal_network(
+                    self.context.session,
+                    vcns_const.InternalEdgePurposes.INTER_EDGE_PURPOSE)
 
-                    # if network is created, clean up
-                    if internal_net:
-                        self.nsxv_plugin.delete_network(self.context,
-                                                        internal_net)
+                # if network is created, clean up
+                if internal_net:
+                    self.nsxv_plugin.delete_network(self.context,
+                                                    internal_net)
 
-                    LOG.exception(_LE("Exception %s while creating internal "
-                                      "network for metadata service"), e)
+                LOG.exception(_LE("Exception %s while creating internal "
+                                  "network for metadata service"), e)
+                return
 
             # Update the new network_id in DB
             nsxv_db.create_nsxv_internal_network(
@@ -382,16 +382,23 @@ class NsxVMetadataProxyHandler:
             return edge_ip
 
         except Exception as e:
-            with excutils.save_and_reraise_exception():
-                LOG.exception(_LE("Exception %s while creating internal edge "
-                                  "for metadata service"), e)
+            LOG.exception(_LE("Exception %s while creating internal edge "
+                              "for metadata service"), e)
 
-                nsxv_db.delete_nsxv_internal_edge(
-                    self.context.session,
-                    rtr_ext_ip)
+            ports = self.nsxv_plugin.get_ports(
+                self.context, filters={'device_id': [rtr_id]})
 
-                if rtr_id:
-                    self.nsxv_plugin.delete_router(self.context, rtr_id)
+            for port in ports:
+                self.nsxv_plugin.delete_port(self.context, port['id'],
+                                             l3_port_check=True,
+                                             nw_gw_port_check=True)
+
+            nsxv_db.delete_nsxv_internal_edge(
+                self.context.session,
+                rtr_ext_ip)
+
+            if rtr_id:
+                self.nsxv_plugin.delete_router(self.context, rtr_id)
 
     def _get_address_groups(self, context, network_id, device_id, is_proxy):
 
