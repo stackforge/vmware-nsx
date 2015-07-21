e1ea2832c75c3761d7234b0ffcb2b8371ef3a53e
Gary Kotton <gkotton@vmware.com>
2015-07-21 05:54:19 -0700

    NSXv: add address pair support
    Adds a limited support for the address pair extension.
    
    This has the following limitations:
    1. Only supports same MAC address for the port
    2. Does not enable the same IP to be configured more than once on
       a network.
    
    This will essentailly only allow a non-neutron IP address to be support
    by a VM.
    
    Change-Id: I3805eb1b1a1a73d85843df07ac8c819ecaa82f86

--- vmware_nsx/plugins/nsx_v/plugin.py
+++ vmware_nsx/plugins/nsx_v/plugin.py
@@ -29,6 +29,7 @@ from neutron.api.v2 import attributes as attr
 from neutron.common import constants
 from neutron.common import exceptions as n_exc
 from neutron.db import agents_db
+from neutron.db import allowedaddresspairs_db as addr_pair_db
 from neutron.db import db_base_plugin_v2
 from neutron.db import external_net_db
 from neutron.db import extraroute_db
@@ -39,6 +40,7 @@ from neutron.db import portbindings_db
 from neutron.db import portsecurity_db
 from neutron.db import quota_db  # noqa
 from neutron.db import securitygroups_db
+from neutron.extensions import allowedaddresspairs as addr_pair
 from neutron.extensions import external_net as ext_net_extn
 from neutron.extensions import l3
 from neutron.extensions import multiprovidernet as mpnet
@@ -83,7 +85,8 @@ ROUTER_SIZE = routersize.ROUTER_SIZE
 VALID_EDGE_SIZES = routersize.VALID_EDGE_SIZES
 
 
-class NsxVPluginV2(agents_db.AgentDbMixin,
+class NsxVPluginV2(addr_pair_db.AllowedAddressPairsMixin,
+                   agents_db.AgentDbMixin,
                    db_base_plugin_v2.NeutronDbPluginV2,
                    rt_rtr.RouterType_mixin,
                    external_net_db.External_net_db_mixin,
@@ -95,6 +98,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
                    vnic_index_db.VnicIndexDbMixin):
 
     supported_extension_aliases = ["agent",
+                                   "allowed-address-pairs",
                                    "binding",
                                    "dvr",
                                    "ext-gw-mode",
@@ -828,6 +832,15 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
             port["port"].update(neutron_db)
             has_ip = self._ip_on_port(neutron_db)
 
+            # allowed address pair checks
+            attrs = port[attr.PORT]
+            if self._check_update_has_allowed_address_pairs(port):
+                if not port_security:
+                    raise addr_pair.AddressPairAndPortSecurityRequired()
+            else:
+                # remove ATTR_NOT_SPECIFIED
+                attrs[addr_pair.ADDRESS_PAIRS] = []
+
             # security group extension checks
             if has_ip:
                 self._ensure_default_security_group_on_port(context, port)
@@ -840,6 +853,11 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
             self._process_portbindings_create_and_update(context,
                                                          port['port'],
                                                          port_data)
+            neutron_db[addr_pair.ADDRESS_PAIRS] = (
+                self._process_create_allowed_address_pairs(
+                    context, neutron_db,
+                    attrs.get(addr_pair.ADDRESS_PAIRS)))
+
         try:
             # Configure NSX - this should not be done in the DB transaction
             # Configure the DHCP Edge service
@@ -852,6 +870,7 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
         return port_data
 
     def update_port(self, context, id, port):
+        attrs = port[attr.PORT]
         port_data = port['port']
         original_port = super(NsxVPluginV2, self).get_port(context, id)
         is_compute_port = self._is_compute_port(original_port)
@@ -927,6 +946,11 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
             self._process_portbindings_create_and_update(context,
                                                          port['port'],
                                                          ret_port)
+
+            if addr_pair.ADDRESS_PAIRS in attrs:
+                self.update_address_pairs_on_port(context, id, port,
+                                                  original_port,
+                                                  ret_port)
 
         if comp_owner_update:
             # Create dhcp bindings, the port is now owned by an instance
@@ -2045,6 +2069,9 @@ class NsxVPluginV2(agents_db.AgentDbMixin,
             session, port['network_id'])
         mac_addr = port['mac_address']
         approved_addrs = [addr['ip_address'] for addr in port['fixed_ips']]
+        # add in the address pair
+        approved_addrs.extend(
+            addr['ip_address'] for addr in port[addr_pair.ADDRESS_PAIRS])
         self.nsx_v.vcns.approve_assigned_addresses(
             sg_policy_id, vnic_id, mac_addr, approved_addrs)
         self.nsx_v.vcns.publish_assigned_addresses(sg_policy_id, vnic_id)
