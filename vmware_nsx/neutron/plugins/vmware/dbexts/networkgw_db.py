6585474cd23e7c9fcddc60d4d3e87cde8ef251eb
Abhishek Raut <rauta@vmware.com>
2015-08-10 09:45:24 -0700

    NSX: Move DB models as part of core vendor decomposition
    As part of core vendor decomposition, this patch moves the models
    from neutron repository to vmware-nsx repository.
    
    Change-Id: I99f358af73fd80246aea0c40ecbeb8ce84f5453d
    Depends-On: I5d5b0acf34417889c2a449f22b92fd105697d90d
    Partial-bug: #1483453

--- vmware_nsx/neutron/plugins/vmware/dbexts/networkgw_db.py
+++ vmware_nsx/neutron/plugins/vmware/dbexts/networkgw_db.py
@@ -17,11 +17,12 @@ from sqlalchemy.orm import exc as sa_orm_exc
 from neutron.api.v2 import attributes
 from neutron.common import exceptions
 from neutron.plugins.common import utils
-from neutron.plugins.vmware.dbexts import nsx_models
 from neutron.plugins.vmware.extensions import networkgw
 from oslo_log import log as logging
 from oslo_utils import uuidutils
 import six
+
+from vmware_nsx.neutron.plugins.vmware.dbexts import nsx_models
 
 LOG = logging.getLogger(__name__)
 DEVICE_OWNER_NET_GW_INTF = 'network:gateway-interface'
