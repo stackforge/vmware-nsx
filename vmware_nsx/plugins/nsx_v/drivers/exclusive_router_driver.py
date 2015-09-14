32ecf7c520ab9e250d14fd32808c23dc453b0926
Amey Bhide <abhide@vmware.com>
2015-09-14 15:51:09 -0700

    Add router-size when creating an exclusive router
    Enables a user to specify router-size while creating an exclusive router for
    creating a load-balancer service
    
    Change-Id: If03b51ce0bc61a8e2aa46de4f1b5869306e1bd7e

--- vmware_nsx/plugins/nsx_v/drivers/exclusive_router_driver.py
+++ vmware_nsx/plugins/nsx_v/drivers/exclusive_router_driver.py
@@ -18,6 +18,7 @@ from neutron.api.v2 import attributes as attr
 
 from vmware_nsx.common import exceptions as nsxv_exc
 from vmware_nsx.common import locking
+from vmware_nsx.common import nsxv_constants
 from vmware_nsx.db import nsxv_db
 from vmware_nsx.plugins.nsx_v.drivers import (
     abstract_router_driver as router_driver)
@@ -32,8 +33,10 @@ class RouterExclusiveDriver(router_driver.RouterBaseDriver):
     def get_type(self):
         return "exclusive"
 
-    def create_router(self, context, lrouter, allow_metadata=True):
-        self.edge_manager.create_lrouter(context, lrouter, dist=False)
+    def create_router(self, context, lrouter, appliance_size,
+                      allow_metadata=True):
+        self.edge_manager.create_lrouter(
+            context, lrouter, dist=False, appliance_size=appliance_size)
         if allow_metadata:
             self.plugin.metadata_proxy_handler.configure_router_edge(
                 lrouter['id'])
