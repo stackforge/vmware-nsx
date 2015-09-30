eaf1fba5faeaf399ba56953daf88d486393957d4
Shih-Hao Li <shihli@vmware.com>
2015-09-29 17:00:23 -0700

    Update L3 codes due to API changes
    Change-Id: Ice5b11e74a7dbb811b249b4e2ded69a4f56237ad

--- vmware_nsx/nsxlib/v3/router.py
+++ vmware_nsx/nsxlib/v3/router.py
@@ -78,7 +78,7 @@ def add_router_link_port(tier1_uuid, tier0_uuid, edge_members):
     # Create Tier0 logical router link port
     tier0_link_port = nsxlib.create_logical_router_port(
         tier0_uuid, display_name=TIER0_ROUTER_LINK_PORT_NAME,
-        resource_type=nsxlib.LROUTERPORT_LINK,
+        resource_type=nsxlib.LROUTERPORT_LINKONTIER0,
         logical_port_id=None,
         address_groups=None)
     linked_logical_port_id = tier0_link_port['id']
@@ -88,7 +88,7 @@ def add_router_link_port(tier1_uuid, tier0_uuid, edge_members):
     # Create Tier1 logical router link port
     nsxlib.create_logical_router_port(
         tier1_uuid, display_name=TIER1_ROUTER_LINK_PORT_NAME,
-        resource_type=nsxlib.LROUTERPORT_LINK,
+        resource_type=nsxlib.LROUTERPORT_LINKONTIER1,
         logical_port_id=linked_logical_port_id,
         address_groups=None,
         edge_cluster_member_index=edge_cluster_member_index)
