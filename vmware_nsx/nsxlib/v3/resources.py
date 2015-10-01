852374104d3665fb059b1c8af8f027716936c6c4
linb <linb@vmware.com>
2015-10-01 11:06:02 +0800

    (WIP)nsx v3 router refactor
    Change-Id: I8ffb28b8656d39161da438b5a174e6d25c35f1e2

--- vmware_nsx/nsxlib/v3/resources.py
+++ vmware_nsx/nsxlib/v3/resources.py
@@ -193,3 +193,123 @@ class LogicalPort(AbstractRESTResource):
         # re-fetch, patch the response and send it again with the
         # new revision_id
         return self._client.update(lport_id, body=lport)
+
+
+class LogicalRouter(AbstractRESTResource):
+
+    @property
+    def uri_segment(self):
+        return 'logical-routers'
+
+
+    def create(display_name, tags, edge_cluster_uuid=None, tier_0=False):
+    # TODO(salv-orlando): If possible do not manage edge clusters in the main
+    # plugin logic.
+    router_type = (nsx_constants.ROUTER_TYPE_TIER0 if tier_0 else
+                   nsx_constants.ROUTER_TYPE_TIER1)
+    body = {'display_name': display_name,
+            'router_type': router_type,
+            'tags': tags}
+    if edge_cluster_uuid:
+        body['edge_cluster_id'] = edge_cluster_uuid
+    return self._client.create(body)
+
+    def delete(self, lrouter_id):
+        return self._client.url_delete('%s?detach=true' % lrouter_id)
+
+    @utils.retry_upon_exception_nsxv3(
+        nsx_exc.StaleRevision,
+        max_attempts=cfg.CONF.nsx_v3.retries)
+    def update(self, lrouter_id, **kwargs):
+        lrouter = self.get(lrouter_id)
+        for k in kwargs:
+            lrouter[k] = kwargs[k]
+        # If revision_id of the payload that we send is older than what NSX has
+        # then we will get a 412: Precondition Failed. In that case we need to
+        # re-fetch, patch the response and send it again with the
+        # new revision_id
+        return self._client.update(lrouter_id, body=lrouter)
+
+
+class LogicalRouterPort(AbstractRESTResource):
+
+    @property
+    def uri_segment(self):
+        return 'logical-router-ports'
+
+    def create(logical_router_id,
+               display_name,
+               resource_type,
+               logical_port_id,
+               address_groups,
+               edge_cluster_member_index=None):
+        body = {'display_name': display_name,
+                'resource_type': resource_type,
+                'logical_router_id': logical_router_id}
+        if address_groups:
+            body['subnets'] = address_groups
+        if resource_type in [LROUTERPORT_UPLINK,
+                             LROUTERPORT_DOWNLINK]:
+            body['linked_logical_switch_port_id'] = logical_port_id
+        elif logical_port_id:
+            body['linked_logical_router_port_id'] = logical_port_id
+        if edge_cluster_member_index:
+            body['edge_cluster_member_index'] = edge_cluster_member_index
+    
+        return self._client.create(body)
+
+    @utils.retry_upon_exception_nsxv3(
+        nsx_exc.StaleRevision,
+        max_attempts=cfg.CONF.nsx_v3.retries)
+    def update(self, logical_port_id, **kwargs):
+        resource = '%s?detach=true' % logical_port_id
+        logical_router_port = self.get(logical_port_id)
+        for k in kwargs:
+            logical_router_port[k] = kwargs[k]
+        # If revision_id of the payload that we send is older than what NSX has
+        # then we will get a 412: Precondition Failed. In that case we need to
+        # re-fetch, patch the response and send it again with the
+        # new revision_id
+        return self._client.update(resource, body=logical_router_port)
+
+    def delete(self, logical_port_id):
+        return self._client.url_delete('%s?detach=true' % logical_port_id)
+
+    def get_logical_router_port_by_ls_id(self, logical_switch_id):
+        resource = '?logical_switch_id=%s' % logical_switch_id
+        router_ports = self._client.url_get(resource)
+        if int(router_ports['result_count']) >= 2:
+            raise nsx_exc.NsxPluginException(
+                err_msg=_("Can't support more than one logical router ports "
+                          "on same logical switch %s ") % logical_switch_id)
+        elif int(router_ports['result_count']) == 1:
+            return router_ports['results'][0]
+        else:
+            err_msg = (_("Logical router link port not found on logical "
+                         "switch %s") % logical_switch_id)
+            raise nsx_exc.ResourceNotFound(manager=client._get_manager_ip(),
+                                           operation=err_msg)
+    
+    def update_logical_router_port_by_ls_id(self, logical_router_id,
+                                            ls_id, **payload):
+        port = self.get_logical_router_port_by_ls_id(ls_id)
+        return self.update(port['id'], **payload)
+
+    def delete_logical_router_port_by_ls_id(self, ls_id):
+        port = self.get_logical_router_port_by_ls_id(ls_id)
+        self.delete(port['id'])
+
+    def get_logical_router_ports_by_router_id(self, logical_router_id):
+        resource = '?logical_router_id=%s' % logical_router_ports
+        logical_router_ports = self._client.url_get(resource)
+        return logical_router_ports['results']
+    
+    def get_tier1_logical_router_link_port(self, logical_router_id):
+        logical_router_ports = self.get_logical_router_ports_by_router_id(
+            logical_router_id)
+        for port in logical_router_ports:
+            if port['resource_type'] == LROUTERPORT_LINKONTIER1:
+                return port
+        raise nsx_exc.ResourceNotFound(
+            manager=client._get_manager_ip(),
+            operation="get router link port")
