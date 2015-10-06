9e1aabbfdbb834b5b5e28ba567c292285933cf49
Amey Bhide <abhide@vmware.com>
2015-10-06 16:16:15 -0700

    NSXv: Check router-type before router-size
    Move router-type validation check before router-size.
    This way if default router-type is set to exclusive; and no router-type
    is specified while router-create; the creation doesn't fail with
    "Type: unspecified" error.
    
    Change-Id: I1256b5e144211fb80252ca989a594192990d4f0e

--- vmware_nsx/plugins/nsx_v/plugin.py
+++ vmware_nsx/plugins/nsx_v/plugin.py
@@ -1414,9 +1414,9 @@ class NsxVPluginV2(addr_pair_db.AllowedAddressPairsMixin,
                     r[ROUTER_SIZE] = appliance_size
 
     def create_router(self, context, router, allow_metadata=True):
-        self._validate_router_size(router)
         r = router['router']
         self._decide_router_type(context, r)
+        self._validate_router_size(router)
         # First extract the gateway info in case of updating
         # gateway before edge is deployed.
         # TODO(berlin): admin_state_up and routes update
