f62ce932f4e4fe9eb187683c63d5069749145cb6
Akash Gangil <gangila@vmware.com>
2015-10-24 10:30:36 -0700

    Framework for debugging nsx-openstack setup
    Purpose of this script is to build a framework which can be leveraged
    to build utilities to help the on-field ops in system debugging. README
    contains all the instructions on how to use it and extend the framework
    by adding new hooks.
    
    Change-Id: I7eabb3afcb1491888445297f33b55bb8d77af87b

---
+++ tools/python-nsxadmin/admin/plugins/common/utils.py
@@ -0,0 +1,33 @@
+# Copyright 2015 VMware, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+
+def output_header(func):
+    """Decorator to demarcate the output of various hooks.
+
+    Based on the callback function name we  add a header to the
+    cli output. Callback name's should follow the convention of
+    component_operation_it_does to leverage the decorator
+    """
+    def func_desc(*args, **kwargs):
+        component = '[{}]'.format(func.func_name.split('_')[0].upper())
+        op_desc = [n.capitalize() for n in func.func_name.split('_')[1:]]
+        LOG.info('==== {} {} ===='.format(component, ' '.join(op_desc)))
+        return func(*args, **kwargs)
+    func_desc.__name__ = func.func_name
+    return func_desc
