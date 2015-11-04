4deada5d5fee1e01c991a0b9744ece82689431e5
Boden R <bodenvmw@gmail.com>
2015-11-04 15:41:44 -0700

    NSX v3 multi-manager
    This patch includes adds multi-manager support to the
    NSX v3 plugin thereby enabling the ability to specify multiple
    NSX managers for the v3 plugin.
    
    This implementation supports the same basic features
    as the MH multi-manager support does including;
    timeouts, retries, keep-alive, etc..
    
    The approach in a nutshell is to introduce a "proxy"
    class which looks like a requests or requests.Session
    object and can be used in place of requests in the
    NSX REST API client. Under the covers this class handles
    management of endpoint selection and connectivity.
    
    Also note that with this patch your devstack local rc / conf
    no longer needs to specify NSX_CONTROLLERS when using
    the v3 plugin. Instead a comma list of managers is supported
    on the NSX_MANAGER devstack var.
    
    Change-Id: I433a4b9ea73de0680d64d86e2f826c092adfba87

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -65,6 +65,7 @@ from vmware_nsx.common import utils
 from vmware_nsx.db import db as nsx_db
 from vmware_nsx.nsxlib import v3 as nsxlib
 from vmware_nsx.nsxlib.v3 import client as nsx_client
+from vmware_nsx.nsxlib.v3 import cluster as nsx_cluster
 from vmware_nsx.nsxlib.v3 import dfw_api as firewall
 from vmware_nsx.nsxlib.v3 import resources as nsx_resources
 from vmware_nsx.nsxlib.v3 import router
@@ -109,6 +110,10 @@ class NsxV3Plugin(addr_pair_db.AllowedAddressPairsMixin,
         super(NsxV3Plugin, self).__init__()
         LOG.info(_("Starting NsxV3Plugin"))
 
+        self._api_cluster = nsx_cluster.NSXClusteredAPI()
+        self._nsx_client = nsx_client.NSX3Client(self._api_cluster)
+        nsx_client._set_default_api_cluster(self._api_cluster)
+
         self.base_binding_dict = {
             pbin.VIF_TYPE: pbin.VIF_TYPE_OVS,
             pbin.VIF_DETAILS: {
@@ -118,7 +123,7 @@ class NsxV3Plugin(addr_pair_db.AllowedAddressPairsMixin,
         self.tier0_groups_dict = {}
         self._setup_dhcp()
         self._start_rpc_notifiers()
-        self._nsx_client = nsx_client.NSX3Client()
+
         self._port_client = nsx_resources.LogicalPort(self._nsx_client)
         self.nsgroup_container, self.default_section = (
             security.init_nsgroup_container_and_default_section_rules())
