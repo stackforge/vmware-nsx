f7af82e059238b97ceaaff9708e86db6e7f206ec
Boden R <bodenvmw@gmail.com>
2015-11-04 15:41:44 -0700

    NSX v3 multi-manager
    This patch adds multi-manager support to the
    NSX v3 plugin thereby enabling the ability to specify multiple
    NSX managers for the v3 plugin.
    
    This implementation supports the same basic features
    as the MH multi-manager support does including;
    timeouts, retries, keep-alive, etc..
    
    The approach in a nutshell is to introduce a "proxy"
    class which looks like a requests or requests.Session
    object and can be used in place of requests in the
    NSX REST API client. Under the covers this class handles
    management of endpoint selection and connectivity.
    
    Also note that with this patch your devstack local rc / conf
    no longer needs to specify NSX_CONTROLLERS when using
    the v3 plugin. Instead a comma list of managers is supported
    on the NSX_MANAGER devstack var.
    
    Change-Id: I433a4b9ea73de0680d64d86e2f826c092adfba87

--- vmware_nsx/tests/unit/nsxlib/v3/test_qos_switching_profile.py
+++ vmware_nsx/tests/unit/nsxlib/v3/test_qos_switching_profile.py
@@ -47,54 +47,58 @@ class NsxLibQosTestCase(nsxlib_testcase.NsxClientTestCase):
         """
         Test creating a qos-switching profile returns the correct response
         """
-        api = self.new_client(client.NSX3Client)
-        with self.mocked_client_bridge(api, nsxlib, 'client') as mocked:
-            nsxlib.create_qos_switching_profile(
-                qos_marking="untrusted", dscp=25, tags=[],
-                name=test_constants_v3.FAKE_NAME,
-                description=test_constants_v3.FAKE_NAME)
+        api = self.mocked_rest_fns(nsxlib, 'client')
 
-            test_client.assert_session_call(
-                mocked.get('post'),
-                'https://1.2.3.4/api/v1/switching-profiles',
-                False,
-                jsonutils.dumps(self._body(qos_marking='UNTRUSTED', dscp=25),
-                                sort_keys=True),
-                _JSON_HEADERS,
-                nsxlib_testcase.NSX_CERT)
+        nsxlib.create_qos_switching_profile(
+            qos_marking="untrusted", dscp=25, tags=[],
+            name=test_constants_v3.FAKE_NAME,
+            description=test_constants_v3.FAKE_NAME)
+
+        test_client.assert_call(
+            'post',
+            api,
+            'https://1.2.3.4/api/v1/switching-profiles',
+            False,
+            jsonutils.dumps(self._body(qos_marking='UNTRUSTED', dscp=25),
+                            sort_keys=True),
+            _JSON_HEADERS,
+            nsxlib_testcase.NSX_CERT)
 
     def test_create_qos_switching_profile_trusted(self):
         """
         Test creating a qos-switching profile returns the correct response
         """
-        api = self.new_client(client.NSX3Client)
-        with self.mocked_client_bridge(api, nsxlib, 'client') as mocked:
-            nsxlib.create_qos_switching_profile(
-                qos_marking="trusted", dscp=0, tags=[],
-                name=test_constants_v3.FAKE_NAME,
-                description=test_constants_v3.FAKE_NAME)
+        api = self.mocked_rest_fns(nsxlib, 'client')
 
-            test_client.assert_session_call(
-                mocked.get('post'),
-                'https://1.2.3.4/api/v1/switching-profiles',
-                False,
-                jsonutils.dumps(self._body(qos_marking='trusted', dscp=0),
-                                sort_keys=True),
-                _JSON_HEADERS,
-                nsxlib_testcase.NSX_CERT)
+        nsxlib.create_qos_switching_profile(
+            qos_marking="trusted", dscp=0, tags=[],
+            name=test_constants_v3.FAKE_NAME,
+            description=test_constants_v3.FAKE_NAME)
+
+        test_client.assert_call(
+            'post',
+            api,
+            'https://1.2.3.4/api/v1/switching-profiles',
+            False,
+            jsonutils.dumps(self._body(qos_marking='trusted', dscp=0),
+                            sort_keys=True),
+            _JSON_HEADERS,
+            nsxlib_testcase.NSX_CERT)
 
     def test_delete_qos_switching_profile(self):
         """
         Test deleting qos-switching-profile
         """
-        api = self.new_client(client.NSX3Client)
-        with self.mocked_client_bridge(api, nsxlib, 'client') as mocked:
-            nsxlib.delete_qos_switching_profile(
-                test_constants_v3.FAKE_QOS_PROFILE['id'])
-            test_client.assert_session_call(
-                mocked.get('delete'),
-                'https://1.2.3.4/api/v1/switching-profiles/%s'
-                % test_constants_v3.FAKE_QOS_PROFILE['id'],
-                False, None,
-                _JSON_HEADERS,
-                nsxlib_testcase.NSX_CERT)
+        api = self.mocked_rest_fns(nsxlib, 'client')
+
+        nsxlib.delete_qos_switching_profile(
+            test_constants_v3.FAKE_QOS_PROFILE['id'])
+
+        test_client.assert_call(
+            'delete',
+            api,
+            'https://1.2.3.4/api/v1/switching-profiles/%s'
+            % test_constants_v3.FAKE_QOS_PROFILE['id'],
+            False, None,
+            _JSON_HEADERS,
+            nsxlib_testcase.NSX_CERT)
