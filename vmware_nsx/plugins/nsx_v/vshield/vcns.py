c337fbedd4f6727e9b457f6f68acd858b612ea84
Roey Chen <roeyc@vmware.com>
2015-11-15 02:45:03 -0800

    Insert new security-group FW sections at the bottom and not at the top
    Each security-group has a dedicated firewall section, till now, when a
    new security-group is created, it's section was inserted at the top of
    the distributed firewall config.
    This is a problem since it leaves no room for a cloud admin to add FW
    sections which precedes the security-group sections.
    This patch makes the needed changes so that new security-groups FW
    sections are created at the bottom (before the default OS cluster
    section), so manually created FW sections at the top will always precedes
    the security-groups sections.
    
    Change-Id: I5e0fa8e35a20f165df4ef257c6ce4bf9e62c4a10

--- vmware_nsx/plugins/nsx_v/vshield/vcns.py
+++ vmware_nsx/plugins/nsx_v/vshield/vcns.py
@@ -517,7 +517,7 @@ class Vcns(object):
         uri = self._build_uri_path(edge_id, BRIDGE)
         return self.do_request(HTTP_DELETE, uri, format='xml', decode=False)
 
-    def create_section(self, type, request):
+    def create_section(self, type, request, insert_before=None):
         """Creates a layer 3 or layer 2 section in nsx rule table.
 
         The method will return the uri to newly created section.
@@ -527,6 +527,8 @@ class Vcns(object):
         else:
             sec_type = 'layer2sections'
         uri = '%s/%s?autoSaveDraft=false' % (FIREWALL_PREFIX, sec_type)
+        if insert_before:
+            uri += '&operation=insert_before&anchorId=%s' % insert_before
         return self.do_request(HTTP_POST, uri, request, format='xml',
                                decode=False, encode=False)
 
