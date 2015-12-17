7bb1b7cd10ae6e95c7cae87f327d47a9eaea1402
Gary Kotton <gkotton@vmware.com>
2015-12-16 23:48:09 -0800

    NSX|V3: fix tags for internal resoucres
    Ensure that internal resource created on the NSX do not have data
    that is not relevant, for example tenant_id.
    
    Closes-bug: #1527084
    (cherry picked from commit aa1e1a7b3e79eca60e10a3612cb0fd60574e9e57)
    
    Conflicts:
    	vmware_nsx/nsxlib/v3/security.py
    	vmware_nsx/plugins/nsx_v3/plugin.py
    
    Change-Id: Ib5f32f55d87fe1a41e7aba4550294fbfb6e4d367

--- vmware_nsx/nsxlib/v3/security.py
+++ vmware_nsx/nsxlib/v3/security.py
@@ -230,8 +230,8 @@ def _init_default_section(name, description, nsgroup_id):
             break
     else:
         section = firewall.create_empty_section(
-            name, description, [nsgroup_id], [])
-        # TODO(roeyc): Add aditional rules to allow IPV6 NDP.
+            name, description, [nsgroup_id],
+            utils.build_v3_api_version_tag())
         block_rule = firewall.get_firewall_rule_dict(
             'Block All', action=firewall.DROP)
 
