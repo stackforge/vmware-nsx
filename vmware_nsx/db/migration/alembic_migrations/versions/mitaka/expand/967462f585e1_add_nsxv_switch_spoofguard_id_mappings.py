3247f4dcea624fb9254784fd2d975eafb2cf7d8e
Abhishek Raut <rauta@vmware.com>
2016-02-22 01:47:36 -0800

    [WIP][NSX-v]: Add support for multiple DVS for VLAN network type
    This patch proposes to use the physical_network attribute
    of provider network API for network type VLAN to consume comma ','
    separated DVS-IDs. This way, admin can provide the multiple DVS-IDs
    associated with a single VLAN network in the provider net create
    command. This will result in creation of port groups and spoofguard
    policies on each of the DVSes.
    
    Spoofguard policies created on each of these DVSes will return a
    unique policy_id. Hence, one neutron network will now be associated
    with multiple policy_ids requiring promotion of policy_id key to a
    Primary key constraint in the nsxv_spoofguard_policy_network_mappings
    table. This patch adds the required migration script.
    
    Usage:
    neutron net-create fancy-network --provider:network_type=vlan
        --provider:segmentation_id=100
        --provider:physical_network=dvs-10,dvs-20
    
    Change-Id: Ifb10e3f34bf0c55d2d7710e804b5de995ec954bd

---
+++ vmware_nsx/db/migration/alembic_migrations/versions/mitaka/expand/967462f585e1_add_nsxv_switch_spoofguard_id_mappings.py
@@ -0,0 +1,41 @@
+# Copyright 2016 VMware, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""add nsxv_switch_spoofguard_id_mappings table
+
+Revision ID: 967462f585e1
+Revises: 4c45bcadccf9
+Create Date: 2016-02-23 18:22:01.998540
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '967462f585e1'
+down_revision = '4c45bcadccf9'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'nsxv_spoofguard_policy_nsx_switch_id_mappings',
+        sa.Column('nsx_switch_id', sa.String(length=36), nullable=False),
+        sa.Column('policy_id', sa.String(length=36), nullable=False),
+        sa.ForeignKeyConstraint(
+            ['policy_id'],
+            ['nsxv_spoofguard_policy_network_mappings.policy_id'],
+            ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('policy_id')
+    )
