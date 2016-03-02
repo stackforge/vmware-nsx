d63c11b07e0e8752deb95ce551ac931dbab66b7c
Tong Liu <tongl@vmware.com>
2016-03-02 23:10:59 +0000

    Tempest: change tempest_lib to tempest.lib
    Due to a recent upstream change, tempest project switched to
    using in-tree tempest lib instead of tempest_lib. We need to
    change our NSX tempest plugin test to use in-tree tempest lib too.
    
    Upstream change:
    https://review.openstack.org/#/c/284911
    
    Fixes-Bug: #1552453
    
    Change-Id: I522d234d948f5b7a6f70dec69c98c44eb5d7582f

--- vmware_nsx_tempest/tests/nsxv/api/test_l2_gateway.py
+++ vmware_nsx_tempest/tests/nsxv/api/test_l2_gateway.py
@@ -14,11 +14,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from tempest.api.network import base
 from tempest import config
 from tempest import test
-from tempest_lib.common.utils import data_utils
-from tempest_lib import decorators
+from tempest.api.network import base
+from tempest.lib import decorators
+from tempest.lib.common.utils import data_utils
 
 from vmware_nsx_tempest.services import base_l2gw
 from vmware_nsx_tempest.services import l2_gateway_client as L2GW
