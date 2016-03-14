ccaad299ad9800cb9f53e93673f0e3778b3a2fef
Roey Chen <roeyc@vmware.com>
2016-03-14 05:58:15 -0700

    Code cleanup for extension 'secgroup-rule-local-ip-prefix'
    Change-Id: I6bf7962396632c7d6052675574a894af14a7f2dc

--- vmware_nsx/db/extended_security_group_rule.py
+++ vmware_nsx/db/extended_security_group_rule.py
@@ -12,7 +12,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-from sqlalchemy.orm import exc
 
 from neutron.api.v2 import attributes as attr
 from neutron.db import db_base_plugin_v2
@@ -37,25 +36,18 @@ class ExtendedSecurityGroupRuleMixin(object):
             raise NotIngressRule()
         return rule_specify_local_ip_prefix
 
-    def _save_extended_rule_properties(self, context, rule):
-        if not attr.is_attr_set(rule.get(ext_loip.LOCAL_IP_PREFIX)):
+    def _process_security_group_rule_properties(self, context,
+                                                rule_res, rule_req):
+        rule_res[ext_loip.LOCAL_IP_PREFIX] = None
+        if not attr.is_attr_set(rule_req.get(ext_loip.LOCAL_IP_PREFIX)):
             return
+
         with context.session.begin(subtransactions=True):
             properties = nsxv_models.NsxvExtendedSecurityGroupRuleProperties(
-                rule_id=rule['id'],
-                local_ip_prefix=rule[ext_loip.LOCAL_IP_PREFIX])
+                rule_id=rule_res['id'],
+                local_ip_prefix=rule_req[ext_loip.LOCAL_IP_PREFIX])
             context.session.add(properties)
-
-    def _get_security_group_rule_properties(self, context, sgr):
-        try:
-            properties = (context.session.query(
-                nsxv_models.NsxvExtendedSecurityGroupRuleProperties).filter_by(
-                    rule_id=sgr['id']).one())
-        except exc.NoResultFound:
-            sgr[ext_loip.LOCAL_IP_PREFIX] = None
-        else:
-            sgr[ext_loip.LOCAL_IP_PREFIX] = properties.local_ip_prefix
-        return sgr
+        rule_res[ext_loip.LOCAL_IP_PREFIX] = rule_req[ext_loip.LOCAL_IP_PREFIX]
 
     db_base_plugin_v2.NeutronDbPluginV2.register_dict_extend_funcs(
         ext_sg.SECURITYGROUPRULES, ['_extend_security_group_rule_with_params'])
