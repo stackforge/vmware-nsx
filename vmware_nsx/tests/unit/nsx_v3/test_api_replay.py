d0d82f02803cdb131bdd8b67251a05923092b8df
Aaron Rosen <aaronorosen@gmail.com>
2016-03-18 14:59:50 -0700

    NSX-v3: Initial framework for api-replay-mode
    This patch includes the initial framework to allow existing
    neutron deployments running different backends to be migrated
    over to the nsx-v3 plugin. The main logic that is required to
    do this is to allow the ability of an id to be specified for
    a given resource. This patch enables this for port_ids only
    for now.
    
    Change-Id: I2ee9778374a8d137e06125f2732524c7c662c002

---
+++ vmware_nsx/tests/unit/nsx_v3/test_api_replay.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2015 OpenStack Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from oslo_config import cfg
+
+from vmware_nsx.tests.unit.nsx_v3 import test_plugin
+
+
+class TestApiReplay(test_plugin.NsxV3PluginTestCaseMixin):
+
+    def setUp(self, plugin=None, ext_mgr=None, service_plugins=None):
+
+        # enables api_replay_mode for these tests
+        cfg.CONF.set_override('api_replay_mode', True, 'nsx_v3')
+        super(TestApiReplay, self).setUp()
+
+    def test_create_port_specify_id(self):
+        specified_port_id = 'e55e762b-d7a1-4b44-b09b-2a34ada56c9f'
+        with self.network() as network:
+            port_res = self._create_port(self.fmt,
+                                         network['network']['id'],
+                                         arg_list=('id',),
+                                         id=specified_port_id)
+            port = self.deserialize(self.fmt, port_res)
+            self.assertEqual(specified_port_id, port['port']['id'])
