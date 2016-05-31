56e36efb50a2a6db0443c8abfa202071c3015ce6
Adit Sarfaty <asarfaty@vmware.com>
2016-05-31 10:03:13 +0300

    NSX-V: support qos policy in network get
    Return the policy id when getting a network
    
    Change-Id: Ib3dbf3910dc1299a8b916788810d3e7ef024a4e8

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -886,7 +886,7 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
             qos_policy_id = port_data[qos_consts.QOS_POLICY_ID]
         elif device_owner.startswith(const.DEVICE_OWNER_COMPUTE_PREFIX):
             # check if the network of this port has a policy
-            qos_policy_id = qos_utils.get_network_policy_id(
+            qos_policy_id = qos_com_utils.get_network_policy_id(
                 context, port_data['network_id'])
         if qos_policy_id:
             qos_profile_id = self._get_qos_profile_id(context, qos_policy_id)
@@ -1249,14 +1249,14 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
             policy_id = updated_port[qos_consts.QOS_POLICY_ID]
         else:
             # Look for the previous QoS policy
-            policy_id = qos_utils.get_port_policy_id(
+            policy_id = qos_com_utils.get_port_policy_id(
                 context, updated_port['id'])
         # If the port is now a 'compute' port (attached to a vm) and
         # Qos policy was not configured on the port directly,
         # try to take it from the ports network
         if policy_id is None and is_new_compute:
             # check if the network of this port has a policy
-            policy_id = qos_utils.get_network_policy_id(
+            policy_id = qos_com_utils.get_network_policy_id(
                 context, updated_port.get('network_id'))
 
         if policy_id is not None:
