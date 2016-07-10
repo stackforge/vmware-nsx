7321689a9994614916f662b119b3d4b6384492ad
Adit Sarfaty <asarfaty@vmware.com>
2016-07-10 14:55:52 +0300

    [Admin-Util] list missing backend networks for NSXv
    Usage example:
    nsxadmin -r missing-networks -o list
    
    Output example:
    missing-networks
    +--------------------------------------+-----------------+--------+
    | neutron_id                           | moref           | dvs_id |
    +--------------------------------------+-----------------+--------+
    | 0d0ad825-4907-4e8f-9bd8-690007dadf3b | dvportgroup-340 | dvs-22 |
    | 6987342e-9d42-4a86-bd4e-034815aa989e | virtualwire-39  | None   |
    +--------------------------------------+-----------------+--------+
    
    Change-Id: I6f060271b5611b1852435ab2f5839a15897c7f76

--- vmware_nsx/shell/admin/plugins/nsxv/resources/networks.py
+++ vmware_nsx/shell/admin/plugins/nsxv/resources/networks.py
@@ -18,6 +18,7 @@ from oslo_serialization import jsonutils
 import xml.etree.ElementTree as et
 
 from vmware_nsx._i18n import _LE, _LI
+from vmware_nsx.db import db as nsx_db
 from vmware_nsx.plugins.nsx_v.vshield.common import exceptions
 from vmware_nsx.shell.admin.plugins.common import constants
 from vmware_nsx.shell.admin.plugins.common import formatters
@@ -26,9 +27,11 @@ from vmware_nsx.shell.admin.plugins.nsxv.resources import utils as utils
 from vmware_nsx.shell import resources as shell
 
 from neutron.callbacks import registry
+from neutron import context
 
 LOG = logging.getLogger(__name__)
 nsxv = utils.get_nsxv_client()
+network_types = ['Network', 'VirtualWire', 'DistributedVirtualPortgroup']
 
 
 def get_networks():
@@ -37,12 +40,24 @@ def get_networks():
     networks = []
     root = et.fromstring(so_list)
     for obj in root.iter('object'):
-        if (obj.find('objectTypeName').text == 'Network' or
-            obj.find('objectTypeName').text == 'VirtualWire' or
-            obj.find('objectTypeName').text == 'DistributedVirtualPortgroup'):
+        if obj.find('objectTypeName').text in network_types:
             networks.append({'type': obj.find('objectTypeName').text,
                              'moref': obj.find('objectId').text,
                              'name': obj.find('name').text})
+    return networks
+
+
+def get_networks_dict():
+    nsxv = utils.get_nsxv_client()
+    so_list = nsxv.get_scoping_objects()
+    networks = {}
+    root = et.fromstring(so_list)
+    for obj in root.iter('object'):
+        if obj.find('objectTypeName').text in network_types:
+            moref = obj.find('objectId').text
+            if moref not in networks:
+                networks[moref] = []
+            networks[moref].append(obj.find('name').text)
     return networks
 
 
@@ -102,9 +117,46 @@ def nsx_update_switch(resource, event, trigger, **kwargs):
         LOG.error(_LE("Possible values: %s"), ', '.join(supported_policies))
 
 
+@admin_utils.output_header
+def list_missing_networks(resource, event, trigger, **kwargs):
+    """List the neutron networks which are missing the backend moref
+    """
+    # get the neutron-nsx networks mapping from DB
+    admin_context = context.get_admin_context()
+    mappings = nsx_db.get_nsx_networks_mapping(admin_context.session)
+    # get the list of backend networks:
+    backend_networks = get_networks_dict()
+    missing_networks = []
+
+    # For each neutron network - check if there is a matching backend network
+    for entry in mappings:
+        nsx_id = entry['nsx_id']
+        dvs_id = entry['dvs_id']
+        if nsx_id not in backend_networks.keys():
+            missing_networks.append({'neutron_id': entry['neutron_id'],
+                                     'moref': nsx_id,
+                                     'dvs_id': dvs_id})
+        elif dvs_id:
+            found = False
+            for netname in backend_networks[nsx_id]:
+                if netname.startswith(dvs_id):
+                    found = True
+            if not found:
+                missing_networks.append({'neutron_id': entry['neutron_id'],
+                                         'moref': nsx_id,
+                                         'dvs_id': dvs_id})
+
+    LOG.info(formatters.output_formatter(constants.MISSING_NETWORKS,
+                                         missing_networks,
+                                         ['neutron_id', 'moref', 'dvs_id']))
+
+
 registry.subscribe(neutron_list_networks,
                    constants.NETWORKS,
                    shell.Operations.LIST.value)
 registry.subscribe(nsx_update_switch,
                    constants.NETWORKS,
                    shell.Operations.NSX_UPDATE.value)
+registry.subscribe(list_missing_networks,
+                   constants.MISSING_NETWORKS,
+                   shell.Operations.LIST.value)
