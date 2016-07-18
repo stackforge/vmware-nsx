58fb1732a793ebfe569167e5053fb18205970b3e
Adit Sarfaty <asarfaty@vmware.com>
2016-07-18 11:14:15 +0300

    (WIP) NSX|v QoS: Add ingress traffic support
    Configure the nsx backend with both ingress & egress BW limits
    
    This patch depends on https://review.openstack.org/#/c/303626/
    
    Change-Id: I48d34486fa646b61a965babc747ba33401cb7571

--- vmware_nsx/tests/unit/services/qos/test_nsxv_notification.py
+++ vmware_nsx/tests/unit/services/qos/test_nsxv_notification.py
@@ -17,6 +17,7 @@ import mock
 from oslo_config import cfg
 from oslo_utils import uuidutils
 
+from neutron.common import constants as n_const
 from neutron import context
 from neutron import manager
 from neutron.objects.qos import policy as policy_object
@@ -70,23 +71,38 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
                        'description': 'Test policy description',
                        'shared': True}}
 
-        self.rule_data = {
+        self.egress_max = 100
+        self.egress_burst = 150
+        self.egress_rule_data = {
             'bandwidth_limit_rule': {
                 'id': uuidutils.generate_uuid(),
-                'max_kbps': 100,
-                'max_burst_kbps': 150,
+                'max_kbps': self.egress_max,
+                'max_burst_kbps': self.egress_burst,
+                'direction': n_const.EGRESS_DIRECTION,
                 'type': qos_consts.RULE_TYPE_BANDWIDTH_LIMIT}}
+        self.ingress_max = 200
+        self.ingress_burst = 250
+        self.ingress_rule_data = {
+            'bandwidth_limit_rule': {
+                'id': uuidutils.generate_uuid(),
+                'max_kbps': self.ingress_max,
+                'max_burst_kbps': self.ingress_burst,
+                'direction': n_const.INGRESS_DIRECTION,
+                'type': qos_consts.RULE_TYPE_BANDWIDTH_LIMIT}}
+        self.dscp_mark_val = 22
         self.dscp_rule_data = {
             'dscp_marking_rule': {
                 'id': uuidutils.generate_uuid(),
-                'dscp_mark': 22,
+                'dscp_mark': self.dscp_mark_val,
                 'type': qos_consts.RULE_TYPE_DSCP_MARKING}}
 
         self.policy = policy_object.QosPolicy(
             self.ctxt, **self.policy_data['policy'])
 
-        self.rule = rule_object.QosBandwidthLimitRule(
-            self.ctxt, **self.rule_data['bandwidth_limit_rule'])
+        self.egress_rule = rule_object.QosBandwidthLimitRule(
+            self.ctxt, **self.egress_rule_data['bandwidth_limit_rule'])
+        self.ingress_rule = rule_object.QosBandwidthLimitRule(
+            self.ctxt, **self.ingress_rule_data['bandwidth_limit_rule'])
         self.dscp_rule = rule_object.QosDscpMarkingRule(
             self.ctxt, **self.dscp_rule_data['dscp_marking_rule'])
 
@@ -98,13 +114,12 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
             'admin_state_up': False,
             'shared': False
         }}
-        self._rules = [self.rule_data['bandwidth_limit_rule']]
         self._dscp_rules = [self.dscp_rule_data['dscp_marking_rule']]
 
         mock.patch('neutron.objects.db.api.create_object',
-            return_value=self.rule_data).start()
+            return_value=self.egress_rule_data).start()
         mock.patch('neutron.objects.db.api.update_object',
-            return_value=self.rule_data).start()
+            return_value=self.egress_rule_data).start()
         mock.patch('neutron.objects.db.api.delete_object').start()
         mock.patch('neutron.objects.db.api.get_object').start()
         mock.patch(
@@ -125,6 +140,36 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
                         return_value=self.policy.id):
             return self._core_plugin.create_network(self.ctxt, self._net_data)
 
+    def _compare_qos_data(self, qos_data, with_dscp=False,
+                          with_egress=False, with_ingress=False):
+        if with_dscp:
+            self.assertEqual(True, qos_data.dscpMarkEnabled)
+            self.assertEqual(self.dscp_mark_val, qos_data.dscpMarkValue)
+        else:
+            self.assertEqual(False, qos_data.dscpMarkEnabled)
+
+        if with_egress:
+            self.assertEqual(True, qos_data.egressBW.bandwidthEnabled)
+            self.assertEqual(self.egress_max * 1024,
+                             qos_data.egressBW.averageBandwidth)
+            self.assertEqual(self.egress_max * 1024,
+                             qos_data.egressBW.peakBandwidth)
+            self.assertEqual(self.egress_burst * 128,
+                             qos_data.egressBW.burstSize)
+        else:
+            self.assertEqual(False, qos_data.egressBW.bandwidthEnabled)
+
+        if with_ingress:
+            self.assertEqual(True, qos_data.ingressBW.bandwidthEnabled)
+            self.assertEqual(self.ingress_max * 1024,
+                             qos_data.ingressBW.averageBandwidth)
+            self.assertEqual(self.ingress_max * 1024,
+                             qos_data.ingressBW.peakBandwidth)
+            self.assertEqual(self.ingress_burst * 128,
+                             qos_data.ingressBW.burstSize)
+        else:
+            self.assertEqual(False, qos_data.ingressBW.bandwidthEnabled)
+
     @mock.patch.object(qos_com_utils, 'update_network_policy_binding')
     @mock.patch.object(dvs.DvsManager, 'update_port_groups_config')
     def test_create_network_with_policy_rule(self,
@@ -134,7 +179,8 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
         # Create a policy with a rule
         _policy = policy_object.QosPolicy(
             self.ctxt, **self.policy_data['policy'])
-        setattr(_policy, "rules", [self.rule, self.dscp_rule])
+        setattr(_policy, "rules", [self.egress_rule, self.ingress_rule,
+                                   self.dscp_rule])
 
         with mock.patch('neutron.services.qos.qos_plugin.QoSPlugin.'
                         'get_policy',
@@ -147,10 +193,12 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
                 self.ctxt, net['id'], self.policy.id)
             # make sure the qos rule was found
             get_rules_mock.assert_called_with(self.ctxt, self.policy.id)
-            # make sure the dvs was updated
+            # make sure the dvs was updated, with the correct values
             self.assertTrue(dvs_update_mock.called)
+            self._compare_qos_data(dvs_update_mock.call_args[0][3],
+                with_dscp=True, with_egress=True, with_ingress=True)
 
-    def _test_rule_action_notification(self, action):
+    def _test_rule_action_notification(self, action, rule_direction):
         with mock.patch.object(qos_com_utils, 'update_network_policy_binding'):
             with mock.patch.object(dvs.DvsManager,
                                    'update_port_groups_config') as dvs_mock:
@@ -159,9 +207,16 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
                 _policy = policy_object.QosPolicy(
                     self.ctxt, **self.policy_data['policy'])
 
+                if rule_direction == n_const.INGRESS_DIRECTION:
+                    rule = self.ingress_rule
+                    rule_data = self.ingress_rule_data
+                else:
+                    rule = self.egress_rule
+                    rule_data = self.egress_rule_data
+
                 # set the rule in the policy data
                 if action != 'create':
-                    setattr(_policy, "rules", [self.rule])
+                    setattr(_policy, "rules", [rule])
 
                 with mock.patch('neutron.services.qos.qos_plugin.QoSPlugin.'
                                 'get_policy',
@@ -175,37 +230,58 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
                         # create/update/delete the rule
                         if action == 'create':
                             self.qos_plugin.create_policy_bandwidth_limit_rule(
-                                self.ctxt, self.policy.id, self.rule_data)
+                                self.ctxt, self.policy.id, rule_data)
                         elif action == 'update':
                             self.qos_plugin.update_policy_bandwidth_limit_rule(
-                                self.ctxt, self.rule.id,
-                                self.policy.id, self.rule_data)
+                                self.ctxt, rule.id,
+                                self.policy.id, rule_data)
                         else:
                             self.qos_plugin.delete_policy_bandwidth_limit_rule(
-                                self.ctxt, self.rule.id, self.policy.id)
+                                self.ctxt, rule.id, self.policy.id)
 
                         # make sure the qos rule was found
                         self.assertTrue(get_rules_mock.called)
                         # make sure the dvs was updated
                         self.assertTrue(dvs_mock.called)
 
-    def test_create_rule_notification(self):
+    def test_create_egress_rule_notification(self):
         """Test the DVS update when a QoS rule, attached to a network,
         is created
         """
-        self._test_rule_action_notification('create')
+        self._test_rule_action_notification('create', n_const.EGRESS_DIRECTION)
 
-    def test_update_rule_notification(self):
+    def test_update_egress_rule_notification(self):
         """Test the DVS update when a QoS rule, attached to a network,
         is modified
         """
-        self._test_rule_action_notification('update')
+        self._test_rule_action_notification('update', n_const.EGRESS_DIRECTION)
 
-    def test_delete_rule_notification(self):
+    def test_delete_egress_rule_notification(self):
         """Test the DVS update when a QoS rule, attached to a network,
         is deleted
         """
-        self._test_rule_action_notification('delete')
+        self._test_rule_action_notification('delete', n_const.EGRESS_DIRECTION)
+
+    def test_create_ingress_rule_notification(self):
+        """Test the DVS update when a QoS rule, attached to a network,
+        is created
+        """
+        self._test_rule_action_notification('create',
+                                            n_const.INGRESS_DIRECTION)
+
+    def test_update_ingress_rule_notification(self):
+        """Test the DVS update when a QoS rule, attached to a network,
+        is modified
+        """
+        self._test_rule_action_notification('update',
+                                            n_const.INGRESS_DIRECTION)
+
+    def test_delete_ingress_rule_notification(self):
+        """Test the DVS update when a QoS rule, attached to a network,
+        is deleted
+        """
+        self._test_rule_action_notification('delete',
+                                            n_const.INGRESS_DIRECTION)
 
     def _test_dscp_rule_action_notification(self, action):
         with mock.patch.object(qos_com_utils, 'update_network_policy_binding'):
@@ -275,3 +351,48 @@ class TestQosNsxVNotification(test_plugin.NsxVPluginV2TestCase,
         is deleted
         """
         self._test_dscp_rule_action_notification('delete')
+
+    def _test_nsxv_rule_init(self, with_dscp=False,
+                             with_egress=False, with_ingress=False):
+        # init the qos policy with the relevant rules
+        _policy = policy_object.QosPolicy(
+            self.ctxt, **self.policy_data['policy'])
+        rules = []
+
+        if with_dscp:
+            rules.append(self.dscp_rule)
+        if with_egress:
+            rules.append(self.egress_rule)
+        if with_ingress:
+            rules.append(self.ingress_rule)
+        setattr(_policy, "rules", rules)
+
+        # translate the policy to the nsxv object
+        with mock.patch('neutron.services.qos.qos_plugin.QoSPlugin.get_policy',
+                        return_value=_policy):
+            with mock.patch('neutron.objects.qos.policy.'
+                            'QosPolicy.get_object',
+                            return_value=_policy):
+                nsxv_rule = qos_utils.NsxVQosRule(context=self.ctxt,
+                                                  qos_policy_id=_policy.id)
+                self._compare_qos_data(
+                    nsxv_rule,
+                    with_dscp=with_dscp,
+                    with_ingress=with_ingress,
+                    with_egress=with_egress)
+
+    def test_nsxv_rule_with_dhcp(self):
+        self._test_nsxv_rule_init(with_dscp=True)
+
+    def test_nsxv_rule_with_ingress_bw(self):
+        self._test_nsxv_rule_init(with_ingress=True)
+
+    def test_nsxv_rule_with_egress_bw(self):
+        self._test_nsxv_rule_init(with_egress=True)
+
+    def test_nsxv_rule_empty(self):
+        self._test_nsxv_rule_init()
+
+    def test_nsxv_rule_all(self):
+        self._test_nsxv_rule_init(
+            with_dscp=True, with_ingress=True, with_egress=True)
