1c9d077ce1e6c0af920ac6c31350f5fee3997860
Kobi Samoray <ksamoray@vmware.com>
2016-07-21 15:14:34 +0300

    (WIP) Edge pool management
    Change-Id: I302fd52592c8ab1135bee3aa34c3cf691cbd50fb

---
+++ vmware_nsx/plugins/nsx_v/drivers/pool_manager.py
@@ -0,0 +1,337 @@
+# Copyright 2016 VMware, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import enum
+import eventlet
+
+from neutron.plugins.common import constants as plugin_const
+from oslo_log import log as logging
+from oslo_utils import excutils
+from oslo_utils import uuidutils
+import six
+from six import moves
+
+from vmware_nsx._i18n import _LE, _LW
+from vmware_nsx.common import locking
+from vmware_nsx.db import nsxv_db
+from vmware_nsx.plugins.nsx_v.vshield.common import constants as vcns_const
+
+
+POOL_DRIVER_LOCKID = 'PoolDriver_%s_%s_%s'
+PM_POOL_WORKERS = 3
+LOG = logging.getLogger(__name__)
+
+
+class PoolMemberState(enum.Enum):
+    PM_CREATING = 'CREATING'
+    PM_SPARE = 'SPARE'
+    PM_ALLOCATED = 'ALLOCATED'
+    PM_ERROR = 'ERROR'
+    PM_DELETING = 'DELETING'
+
+
+@six.add_metaclass(abc.ABCMeta)
+class PoolRegistryBase(object):
+    def __init__(self, elem_size, elem_type, elem_zone):
+        self._elem_size = elem_size
+        self._elem_type = elem_type
+        self._elem_zone = elem_zone
+
+    @abc.abstractmethod
+    def add_member(self, context, id, owner,
+                   state=PoolMemberState.PM_CREATING):
+        pass
+
+    @abc.abstractmethod
+    def use_member(self, context, id, owner):
+        pass
+
+    @abc.abstractmethod
+    def mark_for_creation(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def mark_as_spare_by_owner(self, context, owner, id):
+        pass
+
+    @abc.abstractmethod
+    def mark_as_spare_by_id(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def mark_for_deletion(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def del_member(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def get_spare_members(self, context, states=PoolMemberState.PM_SPARE):
+        pass
+
+    @abc.abstractmethod
+    def get_spare_id(self):
+        pass
+
+
+class PoolManagerBase(object):
+    def __init__(self, context, elem_size, elem_type, elem_zone, low, high,
+                 registry):
+
+        self._elem_size = elem_size
+        self._elem_type = elem_type
+        self._elem_zone = elem_zone
+        self._registry = registry
+        self._low = low
+        self._high = high
+        self._lock_id = POOL_DRIVER_LOCKID % (elem_size, elem_type, elem_zone)
+        self._worker_pool = eventlet.GreenPool(PM_POOL_WORKERS)
+        self.check_thresholds(context)
+
+    @abc.abstractmethod
+    def _create_member(self):
+        """Create member implementation, should return created element id"""
+        pass
+
+    @abc.abstractmethod
+    def _delete_member(self, id):
+        """Delete member implementation"""
+        pass
+
+    @abc.abstractmethod
+    def _init_member(self, id):
+        """Initialize member implementation"""
+        pass
+
+    def check_thresholds(self, context):
+        """Check that the number of spares is within the predefined range and
+        act when it's not
+        """
+        member_count = len(
+            self._registry.get_spare_members(context,
+                                             [PoolMemberState.PM_SPARE,
+                                              PoolMemberState.PM_CREATING]))
+        if member_count < self._low:
+            self.create_members(context, self._low - member_count)
+        elif member_count > self._high:
+            with locking.LockManager.get_lock(self._lock_id):
+                # Recollect member list to conclude which members will be
+                # deleted. Then mark them so they won't be used while delete
+                # is in progress
+                members = self._registry.get_spare_members(
+                    context, [PoolMemberState.PM_SPARE,
+                              PoolMemberState.PM_CREATING])
+                extra_members = members[:(len(members) - self._high)]
+                for member in extra_members:
+                    self._registry.mark_for_deletion(context, member)
+            if extra_members:
+                self.dispose_members(context, extra_members)
+
+    def _async_create_member(self, context, owner):
+        try:
+            id = self._create_member()
+        except Exception:
+            LOG.error(_LE('Failed to deploy pool member for %s'), owner)
+        # Set member to be a spare
+        self._registry.mark_as_spare_by_owner(context, owner, id)
+
+    def _async_delete_member(self, context, id):
+        self._delete_member(id)
+        self._registry.del_member(context, id)
+
+    def _pool_creator(self, context, fake_owners):
+        for fake_owner in fake_owners:
+            self._worker_pool.spawn_n(self._async_create_member,
+                                      context, fake_owner)
+
+    def _pool_disposer(self, context, members):
+        for member in members:
+            self._worker_pool.spawn_n(self._async_delete_member,
+                                      context, member)
+
+    def create_members(self, context, num):
+        """Create multiple spare members asynchronously"""
+
+        # We first generate the DB entries - to avoid concurrency issues
+        fake_owners = []
+        for n in moves.range(num):
+            fake_owner = self._registry.get_spare_id()
+            self._registry.add_member(context, None, fake_owner)
+            fake_owners.append(fake_owner)
+
+        eventlet.spawn_n(self._pool_creator, context, fake_owners)
+
+    def dispose_members(self, context, members):
+        """Dispose multiple spare members asynchronously"""
+        eventlet.spawn_n(self._pool_disposer, context, members)
+
+    def create_member(self, context, state, owner):
+        """Synchronously create member"""
+        id = None
+        with locking.LockManager.get_lock(self._lock_id):
+            try:
+                id = self._create_member()
+                self._registry.add_member(context, id, owner, state)
+            except Exception:
+                with excutils.save_and_reraise_exception():
+                    LOG.exception(
+                        _LE('Failed to create a pool member (size=%(size)s '
+                            'type=%(type)s az=%(az)s) for owner %(owner)s'),
+                        {'size': self._elem_size,
+                         'type': self._elem_type,
+                         'az': self._elem_zone,
+                         'owner': owner})
+        return id
+
+    def delete_member(self, context, id):
+        """Synchronously delete member"""
+        with locking.LockManager.get_lock(self._lock_id):
+            self._registry.mark_for_deletion(context, id)
+        self._delete_member(id)
+        self._registry.del_member(context, id)
+
+    def reuse_member(self, context, id):
+        """Scrap member and pull it back into the spare pool. Check that
+        number of free members is within range while doing so.
+        """
+        self._registry.mark_for_creation(context, id)
+        self._init_member(id)
+        self._registry.mark_as_spare_by_id(context, id)
+        self.check_thresholds(context)
+
+    def get_member(self, context, owner):
+        """Get a free member from pool. Check that number of free members
+        is within range while doing so.
+        """
+        with locking.LockManager.get_lock(self._lock_id):
+            members = self._registry.get_spare_members(context)
+            if members:
+                self._registry.use_member(context, members[0], owner)
+                self.check_thresholds(context)
+                return members[0]
+
+        id = self.create_member(context, PoolMemberState.PM_ALLOCATED, owner)
+        self.check_thresholds(context)
+        return id
+
+
+class NsxvEdgeRegistry(PoolRegistryBase):
+    STATE_DICT = {
+        PoolMemberState.PM_CREATING: plugin_const.PENDING_CREATE,
+        PoolMemberState.PM_SPARE: plugin_const.ACTIVE,
+        PoolMemberState.PM_ALLOCATED: plugin_const.ACTIVE,
+        PoolMemberState.PM_ERROR: plugin_const.ERROR,
+        PoolMemberState.PM_DELETING: plugin_const.PENDING_DELETE}
+
+    def __init__(self, elem_size, elem_type, elem_zone):
+        super(NsxvEdgeRegistry, self).__init__(elem_size, elem_type, elem_zone)
+
+    def get_spare_id(self):
+        return (vcns_const.BACKUP_ROUTER_PREFIX +
+                uuidutils.generate_uuid())[:vcns_const.EDGE_NAME_LEN]
+
+    def add_member(self, context, id, owner,
+                   state=PoolMemberState.PM_CREATING):
+        nsxv_db.add_nsxv_router_binding(
+            context.session, owner, id, None,
+            self.STATE_DICT[state],
+            appliance_size=self._elem_size, edge_type=self._elem_type,
+            availability_zone=self._elem_zone)
+
+    def del_member(self, context, id):
+        binds = nsxv_db.get_nsxv_router_bindings_by_edge(context.session, id)
+        for bind in binds:
+            nsxv_db.delete_nsxv_router_binding(context.session,
+                                               bind['router_id'])
+
+    def use_member(self, context, id, owner):
+        self.del_member(context, id)
+        nsxv_db.add_nsxv_router_binding(
+            context.session, owner, id, None,
+            plugin_const.ACTIVE,
+            appliance_size=self._elem_size, edge_type=self._elem_type,
+            availability_zone=self._elem_zone)
+
+    def _set_spare_state(self, context, binds, state, id=None):
+        if binds:
+            if len(binds) == 1:
+                if binds[0]['router_id'].startswith(
+                        vcns_const.BACKUP_ROUTER_PREFIX):
+                    if id:
+                        nsxv_db.update_nsxv_router_binding(
+                            context.session,
+                            binds[0]['router_id'],
+                            status=state, edge_id=id)
+                    else:
+                        nsxv_db.update_nsxv_router_binding(
+                            context.session,
+                            binds[0]['router_id'],
+                            status=state)
+                    return
+                else:
+                    nsxv_db.delete_nsxv_router_binding(context.session,
+                                                       binds[0]['router_id'])
+            else:
+                LOG.warning(_LW('Found %(len)d bindings for edge %(id)s while '
+                                'setting %(state)s state while expecting one')
+                            % {'len': len(binds), 'id': id, 'state': state})
+                self.del_member(context, id)
+
+        owner = self.get_spare_id()
+        if not id:
+            id = binds[0]['edge_id']
+
+        nsxv_db.add_nsxv_router_binding(
+            context.session, owner, id, None,
+            state,
+            appliance_size=self._elem_size, edge_type=self._elem_type,
+            availability_zone=self._elem_zone)
+
+    def mark_for_creation(self, context, id):
+        binds = nsxv_db.get_nsxv_router_bindings_by_edge(context.session, id)
+        self._set_spare_state(context, binds, plugin_const.PENDING_CREATE)
+
+    def mark_for_deletion(self, context, id):
+        binds = nsxv_db.get_nsxv_router_bindings_by_edge(context.session, id)
+        self._set_spare_state(context, binds, plugin_const.PENDING_DELETE)
+
+    def mark_as_spare_by_owner(self, context, owner, id):
+        binds = nsxv_db.get_nsxv_router_bindings(
+            context.session, filters={'router_id': [owner]})
+        self._set_spare_state(context, binds, plugin_const.ACTIVE, id=id)
+
+    def mark_as_spare_by_id(self, context, id):
+        binds = nsxv_db.get_nsxv_router_bindings_by_edge(context.session, id)
+        self._set_spare_state(context, binds, plugin_const.ACTIVE)
+
+    def get_spare_members(self, context, states=PoolMemberState.PM_SPARE):
+        if not isinstance(states, list):
+            states = [states]
+        db_states = []
+        for state in states:
+            db_states.append(self.STATE_DICT[state])
+
+        binds = nsxv_db.get_nsxv_router_bindings(
+            context.session,
+            filters={'status': db_states,
+                     'availability_zone': [self._elem_zone],
+                     'appliance_size': [self._elem_size],
+                     'edge_type': [self._elem_type]},
+            like_filters={'router_id': vcns_const.BACKUP_ROUTER_PREFIX + "%"})
+
+        b = [b['edge_id'] for b in binds]
+        return b
---
+++ vmware_nsx/tests/unit/nsx_v/test_pool_manager.py
@@ -0,0 +1,272 @@
+# Copyright 2016 VMware, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+import uuid
+
+from neutron import context
+from neutron.tests import base
+import neutron.tests.unit.db.test_db_base_plugin_v2 as test_plugin
+
+from vmware_nsx.db import nsxv_db  # noqa
+from vmware_nsx.plugins.nsx_v.drivers import pool_manager as pmgr
+
+POOL_MIN = 2
+POOL_MAX = 5
+TEST_ITERATIONS = 20
+
+
+class TestPoolRegistry(pmgr.PoolRegistryBase):
+    registry = {}
+    id_dict = {}
+
+    def __init__(self, elem_size, elem_type, elem_zone):
+        super(TestPoolRegistry, self).__init__(elem_size, elem_type, elem_zone)
+
+    def add_member(self, context, id, owner,
+                   state=pmgr.PoolMemberState.PM_CREATING):
+        self.registry[owner] = {'id': id, 'state': state}
+        if id:
+            self.id_dict[id] = owner
+
+    def use_member(self, context, id, owner):
+        old_owner = self.id_dict[id]
+        self.registry[owner] = self.registry[old_owner]
+        self.registry[owner]['state'] = pmgr.PoolMemberState.PM_ALLOCATED
+        del(self.registry[old_owner])
+        self.id_dict[id] = owner
+
+    def mark_for_deletion(self, context, id):
+        owner = self.id_dict[id]
+        self.registry[owner]['state'] = (
+            pmgr.PoolMemberState.PM_DELETING)
+
+    def mark_for_creation(self, context, id):
+        owner = self.id_dict[id]
+        self.registry[owner]['state'] = (
+            pmgr.PoolMemberState.PM_CREATING)
+
+    def mark_as_spare_by_owner(self, context, owner, id):
+        self.registry[owner]['id'] = id
+        self.id_dict[id] = owner
+        self.registry[owner]['state'] = pmgr.PoolMemberState.PM_SPARE
+
+    def mark_as_spare_by_id(self, context, id):
+        owner = self.id_dict[id]
+        new_owner = self.get_spare_id()
+        self.registry[new_owner] = self.registry[owner]
+        del (self.registry[owner])
+        self.registry[new_owner]['state'] = pmgr.PoolMemberState.PM_SPARE
+        self.id_dict[self.registry[new_owner]['id']] = new_owner
+
+    def del_member(self, context, id):
+        owner = self.id_dict[id]
+        del(self.id_dict[id])
+        del(self.registry[owner])
+
+    def get_spare_members(self, context,
+                          states=pmgr.PoolMemberState.PM_SPARE):
+        if not isinstance(states, list):
+            states = [states]
+
+        return [m['id'] for m in self.registry.values()
+                if m['state'] in states]
+
+    def get_spare_id(self):
+        return str(uuid.uuid4())
+
+
+class TestPoolManager(pmgr.PoolManagerBase):
+    def _create_member(self):
+        return str(uuid.uuid4())[:4]
+
+    def _delete_member(self, id):
+        pass
+
+    def _init_member(self, id):
+        pass
+
+
+class PoolManagerBaseTestCase(base.BaseTestCase):
+    def _get_ctx(self):
+        return
+
+    def setUp(self):
+        super(PoolManagerBaseTestCase, self).setUp()
+
+    def _make_pool(self):
+        registry = TestPoolRegistry('size', 'type', 'zone')
+        pool = TestPoolManager(self._get_ctx(), 'size', 'type', 'zone',
+                               POOL_MIN, POOL_MAX, registry)
+
+        return registry, pool
+
+    def _fake_e_spawn_n(self, method, *args):
+        method(*args)
+
+    def _get_reg_total_len(self, reg):
+        return len(reg.registry)
+
+    def _get_reg_ids(self, reg):
+        return list(reg.id_dict.keys())
+
+    def _get_reg_owner_by_id(self, reg, id):
+        return reg.id_dict.get(id)
+
+    def _get_reg_id_by_owner(self, reg, owner):
+        return reg.registry.get(owner, {}).get('id')
+
+    def _get_reg_used_len(self, reg):
+        return len(reg.id_dict)
+
+    def _get_elem_id(self, reg, owner):
+        return reg.registry[owner]['id']
+
+    def test_create_pool(self):
+        with mock.patch.object(pmgr.eventlet, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n), \
+             mock.patch.object(pmgr.eventlet.GreenPool, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n):
+            reg, pool = self._make_pool()
+            self.assertEqual(POOL_MIN, self._get_reg_used_len(reg))
+
+    def test_expand_shrink_pool(self):
+        with mock.patch.object(pmgr.eventlet, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n), \
+             mock.patch.object(pmgr.eventlet.GreenPool, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n):
+            reg, pool = self._make_pool()
+
+            # Load pool
+            received_id = {}
+            for n in range(1, TEST_ITERATIONS):
+                dummy_owner = 'dummy-id-%d' % n
+                member_id = pool.get_member(self._get_ctx(), dummy_owner)
+                received_id[dummy_owner] = member_id
+                self.assertEqual(member_id, self._get_elem_id(reg,
+                                                              dummy_owner))
+                self.assertEqual(POOL_MIN + n, self._get_reg_used_len(reg))
+
+            for n in range(TEST_ITERATIONS - 1, 0, -1):
+                dummy_owner = 'dummy-id-%d' % n
+                pool.reuse_member(self._get_ctx(), received_id[dummy_owner])
+                spares = reg.get_spare_members(self._get_ctx())
+
+                expected_spares = min(TEST_ITERATIONS + POOL_MIN - n, POOL_MAX)
+                self.assertEqual(len(spares), expected_spares)
+
+            # Validate that there are no leftovers in the registry
+            self.assertTrue(self._get_reg_total_len(reg) == POOL_MAX)
+            self.assertTrue(self._get_reg_used_len(reg) == POOL_MAX)
+
+    def test_delete_synchronous(self):
+        with mock.patch.object(pmgr.eventlet, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n), \
+             mock.patch.object(pmgr.eventlet.GreenPool, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n):
+            reg, pool = self._make_pool()
+            # pick registry member for deletion
+            id = self._get_reg_ids(reg)[0]
+            owner = self._get_reg_owner_by_id(reg, id)
+
+            self.assertIsNotNone(owner)
+            self.assertIsNotNone(self._get_reg_id_by_owner(reg, owner))
+            reg_len = self._get_reg_total_len(reg)
+            id_dict_len = self._get_reg_used_len(reg)
+
+            with mock.patch.object(pool, '_delete_member') as mock_del:
+                pool.delete_member(self._get_ctx(), id)
+                self.assertEqual(self._get_reg_total_len(reg), reg_len - 1)
+                self.assertEqual(self._get_reg_used_len(reg), id_dict_len - 1)
+                self.assertIsNone(self._get_reg_owner_by_id(reg, id))
+                self.assertIsNone(self._get_reg_id_by_owner(reg, owner))
+                mock_del.assert_called_once_with(id)
+
+    def test_create_synchronous(self):
+        with mock.patch.object(pmgr.eventlet, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n), \
+             mock.patch.object(pmgr.eventlet.GreenPool, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n):
+            reg, pool = self._make_pool()
+
+            id = pool.create_member(
+                self._get_ctx(), pmgr.PoolMemberState.PM_ALLOCATED, 'dummy')
+
+            # pool should have minimum members plus dummy
+            self.assertIsNotNone(id)
+            self.assertEqual(self._get_reg_total_len(reg), POOL_MIN + 1)
+            self.assertEqual(self._get_reg_used_len(reg), POOL_MIN + 1)
+
+    def test_create_with_empty_pool(self):
+        with mock.patch.object(pmgr.eventlet, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n), \
+             mock.patch.object(pmgr.eventlet.GreenPool, 'spawn_n',
+                               side_effect=self._fake_e_spawn_n):
+            reg, pool = self._make_pool()
+
+            # empty registry
+            reg.registry = {}
+            reg.id_dict = {}
+            id = pool.get_member(self._get_ctx(), 'dummy')
+
+            # pool should have minimum members plus dummy
+            self.assertIsNotNone(id)
+            self.assertEqual(self._get_reg_total_len(reg), POOL_MIN + 1)
+            self.assertEqual(self._get_reg_used_len(reg), POOL_MIN + 1)
+
+
+class PoolManagerNsxvEdgeRegistry(PoolManagerBaseTestCase,
+                                  test_plugin.NeutronDbPluginV2TestCase):
+    def _get_ctx(self):
+        return context.get_admin_context()
+
+    def _get_reg_used_len(self, reg):
+        reg = nsxv_db.get_nsxv_router_bindings(self._get_ctx())
+        n = 0
+        for r in reg:
+            if r['edge_id'] is not None:
+                n += 1
+        return n
+
+    def _get_reg_total_len(self, reg):
+        reg = nsxv_db.get_nsxv_router_bindings(self._get_ctx())
+        return len(reg)
+
+    def _get_elem_id(self, reg, owner):
+        bind = nsxv_db.get_nsxv_router_binding(self._get_ctx().session,
+                                               owner)
+        return bind['edge_id']
+
+    def _get_reg_ids(self, reg):
+        binds = nsxv_db.get_nsxv_router_bindings(self._get_ctx().session)
+        return [b['edge_id'] for b in binds]
+
+    def _get_reg_owner_by_id(self, reg, id):
+        bind = nsxv_db.get_nsxv_router_binding_by_edge(
+            self._get_ctx().session, id)
+        if bind:
+            return bind.get('router_id')
+
+    def _get_reg_id_by_owner(self, reg, owner):
+        bind = nsxv_db.get_nsxv_router_binding(self._get_ctx().session, owner)
+        if bind:
+            return bind['edge_id']
+
+    def _make_pool(self):
+        registry = pmgr.NsxvEdgeRegistry('compact', 'service', 'zone')
+        pool = TestPoolManager(self._get_ctx(), 'size', 'type',
+                               'zone', POOL_MIN, POOL_MAX, registry)
+
+        return registry, pool
