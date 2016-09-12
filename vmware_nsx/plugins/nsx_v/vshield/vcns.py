0136eece795d65007179970f3ef8d6745d05d2af
Adit Sarfaty <asarfaty@vmware.com>
2016-09-12 10:07:04 +0300

    Replace retrying with tenacity
    We are replacing all usages of the retrying package with
    tenacity with an end goal of removing the retrying package
    from our requirements.
    
    This patch also demonstrate how to use the new api to retry only for some
    of the exception error codes
    
    Change-Id: Ie1b082848ac6153d29af7779de914071dc8c1ba5

--- vmware_nsx/plugins/nsx_v/vshield/vcns.py
+++ vmware_nsx/plugins/nsx_v/vshield/vcns.py
@@ -19,12 +19,13 @@ import time
 from oslo_config import cfg
 from oslo_log import log as logging
 from oslo_serialization import jsonutils
-import retrying
 import six
+import tenacity
 import xml.etree.ElementTree as et
 
 from vmware_nsx._i18n import _LE
 from vmware_nsx.common import nsxv_constants
+from vmware_nsx.plugins.nsx_v.vshield.common import constants
 from vmware_nsx.plugins.nsx_v.vshield.common import exceptions
 from vmware_nsx.plugins.nsx_v.vshield.common import VcnsApiClient
 
@@ -82,12 +83,43 @@ CSR = "csr"
 CERTIFICATE = "certificate"
 
 
+def _get_bad_request_error_code(e):
+    """Get the error code out of the exception"""
+    try:
+        desc = et.fromstring(e.response)
+        return int(desc.find('errorCode').text)
+    except Exception:
+        pass
+
+
+def retry_upon_exception_exclude_error_codes(
+    exc, excluded_errors, delay=500, max_delay=4000,
+    max_attempts=cfg.CONF.nsxv.retries):
+    """Retry with the configured exponential delay, unless the exception error
+    code is in the given list
+    """
+    def retry_if_not_error_codes(e):
+        # return True only for BadRequests without error codes or with error
+        # codes not in the exclude list
+        if isinstance(e, exceptions.RequestBad):
+            error_code = _get_bad_request_error_code(e)
+            if error_code and error_code not in excluded_errors:
+                return True
+        return False
+
+    return tenacity.retry(retry=tenacity.retry_if_exception(
+                                retry_if_not_error_codes),
+                          wait=tenacity.wait_exponential(
+                                multiplier=delay, max=max_delay),
+                          stop=tenacity.stop_after_attempt(max_attempts))
+
+
 def retry_upon_exception(exc, delay=500, max_delay=4000,
                          max_attempts=cfg.CONF.nsxv.retries):
-    return retrying.retry(retry_on_exception=lambda e: isinstance(e, exc),
-                          wait_exponential_multiplier=delay,
-                          wait_exponential_max=max_delay,
-                          stop_max_attempt_number=max_attempts)
+    return tenacity.retry(retry=tenacity.retry_if_exception_type(exc),
+                          wait=tenacity.wait_exponential(
+                                multiplier=delay, max=max_delay),
+                          stop=tenacity.stop_after_attempt(max_attempts))
 
 
 class Vcns(object):
@@ -674,7 +706,8 @@ class Vcns(object):
             })
         return {'__enforcementPoints': e_point_list}
 
-    @retry_upon_exception(exceptions.RequestBad)
+    @retry_upon_exception_exclude_error_codes(
+        exceptions.RequestBad, [constants.NSX_ERROR_ALREADY_HAS_SG_POLICY])
     def create_spoofguard_policy(self, enforcement_points, name, enable):
         uri = '%s/policies/' % SPOOFGUARD_PREFIX
 
@@ -795,7 +828,6 @@ class Vcns(object):
         uri = '%s/usermgmt/scopingobjects' % SERVICES_PREFIX
         h, so_list = self.do_request(HTTP_GET, uri, decode=False,
                                      format='xml')
-
         root = et.fromstring(so_list)
         for obj in root.iter('object'):
             if (obj.find('objectTypeName').text == type_name and
