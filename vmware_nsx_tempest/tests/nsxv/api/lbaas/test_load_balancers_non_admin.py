5480f341b2ff646945f2133a557b90b1578b70ef
Alex Kang <akang@vmware.com>
2016-10-12 11:15:11 +0000

    tempest: lbaas l7 switching scenario tests
    1. add lbaas l7 switching clients
    2. enhance to delete l7 resources if present
    3. scenario to support l7 switching
       3.1 checking floatingip assiged to server before ping test
       3.2 enhance test_lbaas_round_robin to support l7
       3.3 test_lbaas_l7_switching_ops only test URL starts_with
           validation.
    
    from: akang-devstack
    
    Change-Id: Ib2ee4ce57d45882d76e25ce7b7bba3d825bf34ab

--- vmware_nsx_tempest/tests/nsxv/api/lbaas/test_load_balancers_non_admin.py
+++ vmware_nsx_tempest/tests/nsxv/api/lbaas/test_load_balancers_non_admin.py
@@ -15,7 +15,6 @@ import netaddr
 from oslo_log import log as logging
 
 from tempest import config
-from tempest.lib import decorators
 from tempest.lib import exceptions
 from tempest import test
 
@@ -240,10 +239,12 @@ class LoadBalancersTest(base.BaseTestCase):
                           tenant_id="&^%123")
 
     @test.attr(type='negative')
-    @decorators.skip_because(bug="1637877")
     @test.idempotent_id('b8c56e4a-9644-4119-8fc9-130841caf662')
     def test_create_load_balancer_invalid_name(self):
-        """Test create load balancer with an invalid name"""
+        """Test create load balancer with an invalid name
+
+        Kilo: @decorators.skip_because(bug="1637877")
+        """
         self.assertRaises(exceptions.BadRequest,
                           self._create_load_balancer,
                           wait=False,
@@ -252,10 +253,12 @@ class LoadBalancersTest(base.BaseTestCase):
                           name='n' * 256)
 
     @test.attr(type='negative')
-    @decorators.skip_because(bug="1637877")
     @test.idempotent_id('d638ae60-7de5-45da-a7d9-53eca4998980')
     def test_create_load_balancer_invalid_description(self):
-        """Test create load balancer with an invalid description"""
+        """Test create load balancer with an invalid description
+
+        Kilo: @decorators.skip_because(bug="1637877")
+        """
         self.assertRaises(exceptions.BadRequest,
                           self._create_load_balancer,
                           wait=False,
@@ -348,10 +351,12 @@ class LoadBalancersTest(base.BaseTestCase):
         self.assertEqual(load_balancer.get('name'), "")
 
     @test.attr(type='negative')
-    @decorators.skip_because(bug="1637877")
     @test.idempotent_id('551be885-215d-4941-8870-651cbc871162')
     def test_update_load_balancer_invalid_name(self):
-        """Test update load balancer with invalid name"""
+        """Test update load balancer with invalid name
+
+        Kilo: @decorators.skip_because(bug="1637877")
+        """
         self.assertRaises(exceptions.BadRequest,
                           self._update_load_balancer,
                           load_balancer_id=self.load_balancer_id,
@@ -372,10 +377,12 @@ class LoadBalancersTest(base.BaseTestCase):
         self.assertEqual(load_balancer_initial, load_balancer_new)
 
     @test.attr(type='negative')
-    @decorators.skip_because(bug="1637877")
     @test.idempotent_id('ab3550c6-8b21-463c-bc5d-e79cbae3432f')
     def test_update_load_balancer_invalid_description(self):
-        """Test update load balancer with invalid description"""
+        """Test update load balancer with invalid description
+
+        Kilo: @decorators.skip_because(bug="1637877")
+        """
         self.assertRaises(exceptions.BadRequest,
                           self._update_load_balancer,
                           load_balancer_id=self.load_balancer_id,
