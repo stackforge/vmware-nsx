1f5694c2a35373093499282e7dcb3f3ebb5b3335
Puneet Arora <apuneet@vmware.com>
2016-11-19 20:30:06 +0000

    [tempest]: Changes done to remove obsolete tempest_id.
    Change-Id: I98d950fbaf685e1c146791b48a90cf6af85e7c87

--- vmware_nsx_tempest/tests/nsxv/scenario/test_dvr_basic_ops.py
+++ vmware_nsx_tempest/tests/nsxv/scenario/test_dvr_basic_ops.py
@@ -106,7 +106,7 @@ class TestDvrBasicOps(manager.NetworkScenarioTest):
     def _setup_network_and_servers(self, **kwargs):
         boot_with_port = kwargs.pop('boot_with_port', False)
         self.security_group = \
-            self._create_security_group(tenant_id=self.tenant_id)
+            self._create_security_group()
         self.network, self.subnet, self.router = self.create_networks(**kwargs)
         self.check_networks()
 
@@ -131,8 +131,7 @@ class TestDvrBasicOps(manager.NetworkScenarioTest):
         network = self._create_network(
             networks_client=networks_client,
             routers_client=routers_client,
-            namestart=namestart,
-            tenant_id=self.tenant_id)
+            namestart=namestart)
 
         router_kwargs = dict(client=routers_client, namestart=namestart)
         for k in kwargs.keys():
@@ -242,7 +241,7 @@ class TestDvrBasicOps(manager.NetworkScenarioTest):
             floating_ip, server)
 
     def _create_new_network(self, create_gateway=False):
-        self.new_net = self._create_network(tenant_id=self.tenant_id)
+        self.new_net = self._create_network()
         if create_gateway:
             self.new_subnet = self._create_subnet(
                 network=self.new_net)
