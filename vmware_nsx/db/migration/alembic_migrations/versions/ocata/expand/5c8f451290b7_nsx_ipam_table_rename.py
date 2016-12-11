ef2531923c418fdd4fd5534a045ffeade98fec58
Adit Sarfaty <asarfaty@vmware.com>
2016-12-11 10:13:21 +0200

    NSX|V3 IPAM support
    The NSX-V3 plugin will use the NSX-V3 backend IPAM.
    An IP pool will be created for each subnet, and port IPs will be allocated
    from this pool.
    The current backend limitation is that we cannot allocate a specific IP,
    so port create/update with fixed_ips will fail.
    
    To enable this option set 'ipam_driver = vmware_nsxv3_ipam' in the
    neutron.conf
    
    Change-Id: I5263555cbb776018a5d01f19d0997fd2adf6483d

---
+++ vmware_nsx/db/migration/alembic_migrations/versions/ocata/expand/5c8f451290b7_nsx_ipam_table_rename.py
@@ -0,0 +1,32 @@
+# Copyright 2016 VMware, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""nsxv_subnet_ipam rename
+
+Revision ID: 5c8f451290b7
+Revises: e816d4fe9d4f
+Create Date: 2016-12-25 11:08:30.300482
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5c8f451290b7'
+down_revision = 'e816d4fe9d4f'
+
+from alembic import op
+
+
+def upgrade():
+    op.rename_table('nsxv_subnet_ipam',
+                    'nsx_subnet_ipam')
