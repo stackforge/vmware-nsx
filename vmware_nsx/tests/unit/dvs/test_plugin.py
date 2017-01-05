38213a3e0385075d64699a01021d67d11dcafc6e
Giridhar Jayavelu <gjayavelu@vmware.com>
2017-01-05 12:36:47 -0800

    [wip] [dvs] Allow vlan-transparent extension.
    Change-Id: I588617e6f87afb2397d37a330e656117209630de

--- vmware_nsx/tests/unit/dvs/test_plugin.py
+++ vmware_nsx/tests/unit/dvs/test_plugin.py
@@ -69,7 +69,7 @@ class DvsTestCase(base.BaseTestCase):
                        return_value='fake-spec')
     def test_add_port_group(self, fake_get_spec):
         self._dvs.add_port_group('fake-uuid', 7)
-        fake_get_spec.assert_called_once_with('fake-uuid', 7)
+        fake_get_spec.assert_called_once_with('fake-uuid', 7, trunk_mode=False)
 
     @mock.patch.object(dvs.DvsManager, '_get_port_group_spec',
                        return_value='fake-spec')
@@ -80,8 +80,10 @@ class DvsTestCase(base.BaseTestCase):
         ):
             self.assertRaises(exp.NeutronException,
                               self._dvs.add_port_group,
-                              'fake-uuid', 7)
-            fake_get_spec.assert_called_once_with('fake-uuid', 7)
+                              'fake-uuid', 7,
+                              trunk_mode=False)
+            fake_get_spec.assert_called_once_with('fake-uuid', 7,
+                                                  trunk_mode=False)
 
     @mock.patch.object(dvs.DvsManager, '_net_id_to_moref',
                        return_value='fake-moref')
@@ -113,7 +115,7 @@ class DvsTestCase(base.BaseTestCase):
         self._dvs.add_port_group(net_id, vlan)
         moref = self._dvs._net_id_to_moref(net_id)
         fake_get_moref.assert_called_once_with(net_id)
-        fake_get_spec.assert_called_once_with(net_id, vlan)
+        fake_get_spec.assert_called_once_with(net_id, vlan, trunk_mode=False)
 
         self._dvs.update_port_groups_config(net_id, moref, None, None)
         fake_update_vxlan.assert_called_once_with(net_id, moref, None, None)
@@ -130,7 +132,7 @@ class DvsTestCase(base.BaseTestCase):
         self._dvs.add_port_group(net_id, vlan)
         moref = self._dvs._net_id_to_moref(net_id)
         fake_get_moref.assert_called_once_with(net_id)
-        fake_get_spec.assert_called_once_with(net_id, vlan)
+        fake_get_spec.assert_called_once_with(net_id, vlan, trunk_mode=False)
 
         self._dvs.update_port_groups_config(net_id, moref, None, None)
         fake_update_net.assert_called_once_with(moref, None, None)
@@ -154,7 +156,8 @@ class NeutronSimpleDvsTest(test_plugin.NeutronDbPluginV2TestCase):
         super(NeutronSimpleDvsTest, self).setUp(plugin=PLUGIN_NAME)
         self._plugin = directory.get_plugin()
 
-    def _create_and_delete_dvs_network(self, network_type='flat', vlan_tag=0):
+    def _create_and_delete_dvs_network(self, network_type='flat', vlan_tag=0,
+                                       trunk_mode=False):
         params = {'provider:network_type': network_type,
                   'provider:physical_network': 'fake-moid',
                   'name': 'fake-name'}
@@ -186,7 +189,8 @@ class NeutronSimpleDvsTest(test_plugin.NeutronDbPluginV2TestCase):
                 else:
                     self.fail()
             if network_type != 'portgroup':
-                mock_add.assert_called_once_with(dvs_id, vlan_tag)
+                mock_add.assert_called_once_with(dvs_id, vlan_tag,
+                                                 trunk_mode=trunk_mode)
             else:
                 mock_add.call_count = 0
                 mock_delete.call_count = 0
