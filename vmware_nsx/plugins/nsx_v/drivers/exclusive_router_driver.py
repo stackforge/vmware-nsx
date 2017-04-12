f3688e2becc8c9777c33761b5b757931a7376065
Roey Chen <roeyc@vmware.com>
2017-04-12 08:50:34 -0700

    NSXv: Adding more rotuer driver notifications
    Change Ifbfa959526c5b5e3add94708d96517935e5cb3fa introduces event
    notifications specific to NSXv routers.
    Currently we're missing two notification calls in attach_router
    and detach_router for exclusive and shared rotuers, these are called
    when updating from one router type to the other.
    
    This patch also relocates the notification (before router services
    removed from edge) in router_interface_remove to be called before
    actually deleting the interface on the edge.
    This is needed since some callbacks require the interface to exists in order to
    complete it's action (e.g - cleaning RouterID from the
    edge dynamic routing section)
    
    Change-Id: Ifb11abfc315faf7b34f2fb35972744b855138cb9

--- vmware_nsx/plugins/nsx_v/drivers/exclusive_router_driver.py
+++ vmware_nsx/plugins/nsx_v/drivers/exclusive_router_driver.py
@@ -78,6 +78,7 @@ class RouterExclusiveDriver(router_driver.RouterBaseDriver):
 
     def detach_router(self, context, router_id, router):
         LOG.debug("Detach exclusive router id %s", router_id)
+        self._notify_before_router_edge_association(context, router)
         self.edge_manager.unbind_router_on_edge(context, router_id)
         if self.plugin.metadata_proxy_handler:
             az = self.get_router_az_by_id(context, router_id)
@@ -192,7 +193,7 @@ class RouterExclusiveDriver(router_driver.RouterBaseDriver):
 
             # Update static routes in all.
             self.plugin._update_routes(context, router_id, newnexthop)
-        if new_ext_net_id:
+        if new_ext_net_id or force_update:
             self._notify_after_router_edge_association(context, router)
 
     def add_router_interface(self, context, router_id, interface_info):
