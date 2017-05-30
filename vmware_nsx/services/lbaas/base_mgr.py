9d631fc4a379b8b0270afd2e1cab12a8dd00f173
Tong Liu <tongl@vmware.com>
2017-05-30 13:57:54 -0700

    NSXv3: Neutron LBaaS nsxv3 support
    This patch adds neutron lBaaS nsxv3 support. The refactor has been
    done in neutron-lbaas to share the same driver for nsxv and nsxv3.
    (I7308035d38f2ab15a85096ec30388ef5c3f56ca3). Also load balancer
    API wrapper for nsxv3 also has been added to vmware-nsxlib in
    (I0fc80e20551e0994888d8c222a9a620dcb2f6e32).
    
    This patch implements the functionality of the following lbaas
    resources:
      - loadbalancer
      - listener
      - pool
      - member
      - healthmonitor
    
    Note that layer7 support is not in this patch. It will be
    supported in another path.
    
    Change-Id: I43473f41343e7b7499bf3ebdaf0a51fd2644509a

--- vmware_nsx/services/lbaas/base_mgr.py
+++ vmware_nsx/services/lbaas/base_mgr.py
@@ -13,13 +13,25 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_log import log as logging
+from neutron_lib.callbacks import events
+from neutron_lib.callbacks import registry
+from neutron_lib.callbacks import resources
+from neutron_lib import constants as lib_const
 from neutron_lib.plugins import constants as plugin_const
 from neutron_lib.plugins import directory
+
+from vmware_nsx.common import exceptions as nsx_exc
+from vmware_nsx.plugins.nsx_v3 import utils as v3_utils
+from vmware_nsxlib.v3 import nsx_constants as consts
+
+LOG = logging.getLogger(__name__)
 
 
 class LoadbalancerBaseManager(object):
     _lbv2_driver = None
     _core_plugin = None
+    _flavor_plugin = None
 
     def __init__(self):
         super(LoadbalancerBaseManager, self).__init__()
@@ -45,6 +57,14 @@ class LoadbalancerBaseManager(object):
 
         return LoadbalancerBaseManager._core_plugin
 
+    @property
+    def flavor_plugin(self):
+        if not LoadbalancerBaseManager._flavor_plugin:
+            LoadbalancerBaseManager._flavor_plugin = (
+                self._get_plugin(plugin_const.FLAVORS))
+
+        return LoadbalancerBaseManager._flavor_plugin
+
 
 class EdgeLoadbalancerBaseManager(LoadbalancerBaseManager):
 
@@ -55,3 +75,29 @@ class EdgeLoadbalancerBaseManager(LoadbalancerBaseManager):
     @property
     def vcns(self):
         return self.vcns_driver.vcns
+
+
+class Nsxv3LoadbalancerBaseManager(LoadbalancerBaseManager):
+
+    def __init__(self):
+        super(Nsxv3LoadbalancerBaseManager, self).__init__()
+        self.nsxlib = v3_utils.get_nsxlib_wrapper()
+
+        self.backend_support = True
+        registry.subscribe(
+            self.check_backend_version,
+            resources.PROCESS, events.BEFORE_SPAWN)
+
+    def check_backend_version(self, resource, event, trigger, **kwargs):
+        if not self.nsxlib.feature_supported(consts.FEATURE_LOAD_BALANCER):
+            # loadbalancer is not supported
+            LOG.warning("LBaaS is not supported by the NSX backend (version "
+                        "%s): loadbalancer is not supported",
+                        self.nsxlib.get_version())
+            self.backend_support = False
+
+    def validate_backend_version(self):
+        # prevent loadbalancer actions if the backend does not support it
+        if not self.backend_support:
+            LOG.error("The NSX backend does not support loadbalancer")
+            raise nsx_exc.InvalidVersion(version=self.nsxlib.get_version())
