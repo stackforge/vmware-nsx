8ca1ad186042706ff11faaae3d7749cb1423554f
Tong Liu <tongl@vmware.com>
2017-05-30 13:57:54 -0700

    NSXv3: Neutron LBaaS nsxv3 support
    This patch adds neutron lBaaS nsxv3 support. The refactor has been
    done in neutron-lbaas to share the same driver for nsxv and nsxv3.
    (I7308035d38f2ab15a85096ec30388ef5c3f56ca3). Also load balancer
    API wrapper for nsxv3 also has been added to vmware-nsxlib in
    (I0fc80e20551e0994888d8c222a9a620dcb2f6e32).
    
    This patch implements the functionality of the following lbaas
    resources:
      - loadbalancer
      - listener
      - pool
      - member
      - healthmonitor
    
    Note that layer7 support is not in this patch. It will be
    supported in another path.
    
    Change-Id: I43473f41343e7b7499bf3ebdaf0a51fd2644509a

---
+++ vmware_nsx/services/lbaas/nsx_v3/lb_driver_v2.py
@@ -0,0 +1,38 @@
+# Copyright 2017 VMware, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from oslo_log import helpers as log_helpers
+from oslo_log import log as logging
+
+from vmware_nsx.services.lbaas.nsx_v3 import healthmonitor_mgr as hm_mgr
+from vmware_nsx.services.lbaas.nsx_v3 import listener_mgr
+from vmware_nsx.services.lbaas.nsx_v3 import loadbalancer_mgr as lb_mgr
+from vmware_nsx.services.lbaas.nsx_v3 import member_mgr
+from vmware_nsx.services.lbaas.nsx_v3 import pool_mgr
+
+LOG = logging.getLogger(__name__)
+
+
+class EdgeLoadbalancerDriverV2(object):
+    @log_helpers.log_method_call
+    def __init__(self):
+        super(EdgeLoadbalancerDriverV2, self).__init__()
+
+        self.loadbalancer = lb_mgr.EdgeLoadBalancerManager()
+        self.listener = listener_mgr.EdgeListenerManager()
+        self.pool = pool_mgr.EdgePoolManager()
+        self.member = member_mgr.EdgeMemberManager()
+        self.healthmonitor = hm_mgr.EdgeHealthMonitorManager()
