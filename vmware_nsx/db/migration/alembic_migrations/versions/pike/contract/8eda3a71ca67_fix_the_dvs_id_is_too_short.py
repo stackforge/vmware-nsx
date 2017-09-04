1b0be2e1c2459dfad6909df1a2e2a077fbcb1513
zhangdaolong <zhangdaolong@fiberhome.com>
2017-09-04 16:52:30 +0800

    Fix data too short for column 'dvs_id'
    The dvs_id is composed of netork name and UUID(length is 36),
    so the length should be greater than 36.
    dvs_testeee-5af71cd6-d420-4ef6-abe4-bd850882c755
    |          ||                                  |
    network name         UUID(length is 36)
    
    I think this cause the following error.
    ----------------------------------
    107, in raise_mysql_exception 2017-08-29 16:13:44.102 5071 ERROR
    neutron.api.v2.resource raise errorclass(errno, errval) 2017-08-29
    16:13:44.102 5071 ERROR neutron.api.v2.resource DBDataError:
    (pymysql.err.DataError) (1406, u"Data too long for column 'dvs_id' at
    row 1") [SQL: u'INSERT INTO neutron_nsx_network_mappings (created_at,
    updated_at, neutron_id, nsx_id, dvs_id) VALUES (%(created_at)s,
    %(updated_at)s, %(neutron_id)s, %(nsx_id)s, %(dvs_id)s)'] [parameters:
    %{'dvs_id': u'dvs_testeee-5af71cd6-d420-4ef6-abe4-bd850882c755',
    %'created_at': datetime.datetime(2017, 8, 29, 8, 13, 43, 985922),
    %'nsx_id': 123, 'updated_at': None, 'neutron_id':
    %'5af71cd6-d420-4ef6-abe4-bd850882c755'}]
    
    Change-Id: I52fdcbe759ea455742707e688c6d6b86601f49e8

---
+++ vmware_nsx/db/migration/alembic_migrations/versions/pike/contract/8eda3a71ca67_fix_the_dvs_id_is_too_short.py
@@ -0,0 +1,39 @@
+# Copyright 2017 VMware, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""fix the dvs_id is too short
+
+Revision ID: 8eda3a71ca67
+Revises: 8c0a81a07691
+Create Date: 2017-09-04 15:52:23.005265
+"""
+
+# revision identifiers, used by Alembic.
+revision = '8eda3a71ca67'
+down_revision = '8c0a81a07691'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# milestone identifier, used by neutron-db-manage
+neutron_milestone = [migration.PIKE]
+
+
+def upgrade():
+    op.alter_column('neutron_nsx_network_mappings', 'dvs_id',
+                    nullable=True, existing_type=sa.String(length=255),
+                    existing_nullable=False)
+
+
