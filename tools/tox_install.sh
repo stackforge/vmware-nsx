9ef643f6c99ac6627d7e366a7624ce98d37745b9
Boden R <bodenvmw@gmail.com>
2017-10-12 12:02:49 -0600

    DNM: zuul v3 gate changes
    DO NOT MERGE
    
    This patch updates our project to work with zuul v3.
    
    Change-Id: Ic0d25e9f9e6d1fe7cba7cae93e66dbbc08c12071

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-DIR=$(dirname $0)
-${DIR}/tox_install_project.sh neutron neutron $*
-${DIR}/tox_install_project.sh networking-l2gw networking_l2gw $*
-${DIR}/tox_install_project.sh networking-sfc networking_sfc $*
-${DIR}/tox_install_project.sh neutron-lbaas neutron_lbaas $*
-${DIR}/tox_install_project.sh vmware-nsxlib vmware_nsxlib $*
-${DIR}/tox_install_project.sh neutron-fwaas neutron_fwaas $*
-${DIR}/tox_install_project.sh neutron-dynamic-routing neutron-dynamic-routing $*
-${DIR}/tox_install_project.sh neutron-vpnaas neutron-vpnaas $*
-
 CONSTRAINTS_FILE=$1
 shift
 
