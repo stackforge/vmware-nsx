6499e2b98afa7de166696f2e475f8aa13c491ea5
Adit Sarfaty <asarfaty@vmware.com>
2017-11-08 10:16:17 +0200

    [WIP] NSX|V3: VPNaaS support
    Depends-on: I53bf97a304c07cd9b1b8c82192225810d3ef1966
    Change-Id: Iad3778c1d826ae67f1b602625f5be0fe2f4c8fe3

--- vmware_nsx/plugins/nsx_v3/plugin.py
+++ vmware_nsx/plugins/nsx_v3/plugin.py
@@ -25,6 +25,8 @@ from neutron_lib.api.validators import availability_zone as az_validator
 from neutron_lib.exceptions import allowedaddresspairs as addr_exc
 from neutron_lib.exceptions import l3 as l3_exc
 from neutron_lib.exceptions import port_security as psec_exc
+from neutron_lib.plugins import constants as plugin_const
+from neutron_lib.plugins import directory
 from neutron_lib.services.qos import constants as qos_consts
 
 from neutron.api.rpc.agentnotifiers import dhcp_rpc_agent_api
@@ -3117,7 +3119,8 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
         return (ipaddress, netmask, nexthop)
 
-    def _get_tier0_uuid_by_net(self, context, network_id):
+    def _get_tier0_uuid_by_router(self, context, router):
+        network_id = router.gw_port_id and router.gw_port.network_id
         if not network_id:
             return
         network = self.get_network(context, network_id)
@@ -3128,10 +3131,8 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
     def _update_router_gw_info(self, context, router_id, info):
         router = self._get_router(context, router_id)
-        org_ext_net_id = router.gw_port_id and router.gw_port.network_id
-        org_tier0_uuid = self._get_tier0_uuid_by_net(context, org_ext_net_id)
+        org_tier0_uuid = self._get_tier0_uuid_by_router(context, router)
         org_enable_snat = router.enable_snat
-        new_ext_net_id = info and info.get('network_id')
         orgaddr, orgmask, _orgnexthop = (
             self._get_external_attachment_info(
                 context, router))
@@ -3151,8 +3152,7 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
         super(NsxV3Plugin, self)._update_router_gw_info(
             context, router_id, info, router=router)
 
-        new_ext_net_id = router.gw_port_id and router.gw_port.network_id
-        new_tier0_uuid = self._get_tier0_uuid_by_net(context, new_ext_net_id)
+        new_tier0_uuid = self._get_tier0_uuid_by_router(context, router)
         new_enable_snat = router.enable_snat
         newaddr, newmask, _newnexthop = (
             self._get_external_attachment_info(
@@ -3592,9 +3592,19 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
         (but not both) and include the source/dest nsx logical port.
         """
         extra_rules = []
+
+        # if it is a single port, the source/dest is this logical port
+        if port_id:
+            _net_id, nsx_port_id = nsx_db.get_nsx_switch_and_port_id(
+                context.session, port_id)
+            port_target = [{'target_type': 'LogicalPort',
+                            'target_id': nsx_port_id}]
+        else:
+            port_target = None
+        elv_ctx = context.elevated()
+
         # DHCP relay rules:
         # get the list of relevant relay servers
-        elv_ctx = context.elevated()
         if port_id:
             relay_servers = self._get_port_relay_servers(elv_ctx, port_id)
         else:
@@ -3610,14 +3620,6 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
         # Add rules to allow dhcp traffic relay servers
         if relay_servers:
-            # if it is a single port, the source/dest is this logical port
-            if port_id:
-                _net_id, nsx_port_id = nsx_db.get_nsx_switch_and_port_id(
-                    context.session, port_id)
-                port_target = [{'target_type': 'LogicalPort',
-                                'target_id': nsx_port_id}]
-            else:
-                port_target = None
             # translate the relay server ips to the firewall format
             relay_target = []
             if self.fwaas_callbacks:
@@ -3642,6 +3644,16 @@ class NsxV3Plugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
                 'sources': port_target,
                 'services': dhcp_services,
                 'direction': 'OUT'})
+
+        # VPN rules:
+        vpn_plugin = directory.get_plugin(plugin_const.VPN)
+        if vpn_plugin:
+            vpn_driver = vpn_plugin.drivers[vpn_plugin.default_provider]
+            vpn_rules = (
+                vpn_driver._generate_ipsecvpn_firewall_rules(
+                    context, router_id))
+            if vpn_rules:
+                extra_rules.extend(vpn_rules)
 
         return extra_rules
 
