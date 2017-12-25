42ae1073bfb45a96d740a2efc9211ecc0bbcea55
Adit Sarfaty <asarfaty@vmware.com>
2017-12-25 09:07:39 +0200

    NSX|v+v3: Prevent adding default route to router
    Both NSX backend does not support adding a static route with destination
    0.0.0.0/0.
    The prefered way to change the default gw is to change the gw-ip of the
    external subnet.
    
    Change-Id: Ibb4f81a484de48f7ea65cb2bb6968e55eae087ad

--- vmware_nsx/plugins/common/plugin.py
+++ vmware_nsx/plugins/common/plugin.py
@@ -280,3 +280,62 @@ class NsxPluginBase(db_base_plugin_v2.NeutronDbPluginV2,
         if validators.is_attr_set(fixed_ip_list) and len(fixed_ip_list) > 1:
             msg = _('Exceeded maximum amount of fixed ips per port')
             raise n_exc.InvalidInput(error_message=msg)
+<<<<<<< HEAD
+=======
+
+    def _extract_external_gw(self, context, router, is_extract=True):
+        r = router['router']
+        gw_info = constants.ATTR_NOT_SPECIFIED
+        # First extract the gateway info in case of updating
+        # gateway before edge is deployed.
+        if 'external_gateway_info' in r:
+            gw_info = r.get('external_gateway_info', {})
+            if is_extract:
+                del r['external_gateway_info']
+            network_id = (gw_info.get('network_id') if gw_info
+                          else None)
+            if network_id:
+                ext_net = self._get_network(context.elevated(), network_id)
+                if not ext_net.external:
+                    msg = (_("Network '%s' is not a valid external network") %
+                           network_id)
+                    raise n_exc.BadRequest(resource='router', msg=msg)
+
+                subnets = self._get_subnets_by_network(context.elevated(),
+                                                       network_id)
+                if not subnets:
+                    msg = _("Cannot update gateway on Network '%s' "
+                            "with no subnet") % network_id
+                    raise n_exc.BadRequest(resource='router', msg=msg)
+        return gw_info
+
+    def get_subnets_by_network(self, context, network_id):
+        return [self._make_subnet_dict(subnet_obj) for subnet_obj in
+                self._get_subnets_by_network(context.elevated(), network_id)]
+
+    def _validate_routes(self, context, router_id, routes):
+        super(NsxPluginBase, self)._validate_routes(
+            context, router_id, routes)
+        # do not allow adding a default route. NSX-v/v3 don't support it
+        for route in routes:
+            if route.get('destination') == '0.0.0.0/0':
+                msg = _("Cannot set a default route using static routes")
+                raise n_exc.BadRequest(resource='router', msg=msg)
+
+
+# Register the callback
+def _validate_network_has_subnet(resource, event, trigger, **kwargs):
+    network_id = kwargs.get('network_id')
+    subnets = kwargs.get('subnets')
+    if not subnets:
+        msg = _('No subnet defined on network %s') % network_id
+        raise n_exc.InvalidInput(error_message=msg)
+
+
+def subscribe():
+    registry.subscribe(_validate_network_has_subnet,
+                       resources.ROUTER_GATEWAY, events.BEFORE_CREATE)
+
+
+subscribe()
+>>>>>>> 09784cc... NSX|v+v3: Prevent adding default route to router
