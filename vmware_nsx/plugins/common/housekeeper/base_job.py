2a333c2cfe5a5ab42169ad7ad73468764a19f217
Gary Kotton <gkotton@vmware.com>
2018-01-01 05:24:15 -0800

    TVD: support housekeeper for TVD
    Ensure that TVD is supported.
    
    Change-Id: I94f99d13e716f99fd6823218dacad8293958ee81

--- vmware_nsx/plugins/common/housekeeper/base_job.py
+++ vmware_nsx/plugins/common/housekeeper/base_job.py
@@ -19,14 +19,28 @@ from neutron_lib.plugins import directory
 from oslo_log import log
 import six
 
+from vmware_nsx.extensions import projectpluginmap
+
 LOG = log.getLogger(__name__)
 
 
 @six.add_metaclass(abc.ABCMeta)
 class BaseJob(object):
+
+    _core_plugin = None
+
     def __init__(self, readonly):
         self.readonly = readonly
-        self.plugin = directory.get_plugin()
+
+    @property
+    def plugin(self):
+        if not self._core_plugin:
+            self._core_plugin = directory.get_plugin()
+            if self._core_plugin.is_tvd_plugin():
+                # get the plugin that match this driver
+                self._core_plugin = self._core_plugin.get_plugin_by_type(
+                    projectpluginmap.NsxPlugins.NSX_V)
+        return self._core_plugin
 
     @abc.abstractmethod
     def get_name(self):
