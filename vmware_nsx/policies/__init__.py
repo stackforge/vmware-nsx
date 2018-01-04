e20929658db6bfe4194f82a1cba622034251ee6f
Michal Kelner Mishali <mkelnermishal@vmware.com>
2018-01-04 09:10:00 +0200

    Moving policies into code
    files renaming and changing init
    
    adding license header to all files
    
    refactoring and removing dup code (neutron)
    
    adding ports and network-profile that are not in neutron
    and are needed
    
    Depends-On: I6f8f8555e8560bbd66f5b033e7aed95c1138cf5c
    
    WIP
    
    adding another one for routers
    
    Change-Id: Ib26048f94142154788293743a7ab105b4570a982
    Signed-off-by: Michal Kelner Mishali <mkelnermishal@vmware.com>

---
+++ vmware_nsx/policies/__init__.py
@@ -0,0 +1,31 @@
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+
+import itertools
+import os
+
+from vmware_nsx.policies import dynamic_routing
+from vmware_nsx.policies import flow_classifier
+from vmware_nsx.policies import ports
+from vmware_nsx.policies import security_groups
+
+NSX_EXT_PATH = os.path.join(os.path.dirname(__file__), 'extensions')
+
+
+def list_rules():
+    return itertools.chain(
+        dynamic_routing.list_rules(),
+        flow_classifier.list_rules(),
+        ports.list_rules(),
+        security_groups.list_rules(),
+    )
