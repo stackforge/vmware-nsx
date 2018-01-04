ea7387d466ccbeabcd0e120f1d924db0dc19dc6e
Michal Kelner Mishali <mkelnermishal@vmware.com>
2018-01-04 09:10:00 +0200

    Moving policies into code
    Modifying according to GaryK's comments
    not all of them, still investigating some
    
    files renaming and changing init
    
    adding license header to all files
    
    refactoring and removing dup code (neutron)
    
    adding ports and network-profile that are not in neutron
    and are needed
    
    Depends-On: I6f8f8555e8560bbd66f5b033e7aed95c1138cf5c
    
    WIP
    
    adding another one for routers
    
    Change-Id: Ib26048f94142154788293743a7ab105b4570a982
    Signed-off-by: Michal Kelner Mishali <mkelnermishal@vmware.com>

---
+++ vmware_nsx/policies/dynamic_routing.py
@@ -0,0 +1,64 @@
+# Copyright 2017 VMware, Inc.
+# All Rights Reserved
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_policy import policy
+
+
+rules = [
+    policy.RuleDefault('get_bgp_speaker',
+                       'rule:admin_only',
+                       description='Rule for getting BGP speaker'),
+    policy.RuleDefault('create_bgp_speaker',
+                       'rule:admin_only',
+                       description='Rule for creating BGP speaker'),
+    policy.RuleDefault('update_bgp_speaker',
+                       'rule:admin_only',
+                       description='Rule for updating BGP speaker'),
+    policy.RuleDefault('delete_bgp_speaker',
+                       'rule:admin_only',
+                       description='Rule for deleting BGP speaker'),
+    policy.RuleDefault('get_bgp_peer',
+                       'rule:admin_only',
+                       description='Rule for getting BGP peer'),
+    policy.RuleDefault('create_bgp_peer',
+                       'rule:admin_only',
+                       description='Rule for creating BGP peer'),
+    policy.RuleDefault('update_bgp_peer',
+                       'rule:admin_only',
+                       description='Rule for updating BGP peer'),
+    policy.RuleDefault('delete_bgp_peer',
+                       'rule:admin_only',
+                       description='Rule for deleting BGP peer'),
+    policy.RuleDefault('add_bgp_peer',
+                       'rule:admin_only',
+                       description='Rule for adding BGP peer'),
+    policy.RuleDefault('remove_bgp_peer',
+                       'rule:admin_only',
+                       description='Rule for removing BGP peer'),
+    policy.RuleDefault('add_gateway_network',
+                       'rule:admin_only',
+                       description='Rule for adding gateway network'),
+    policy.RuleDefault('remove_gateway_network',
+                       'rule:admin_only',
+                       description='Rule for removing gateway network'),
+    policy.RuleDefault('get_advertised_routes',
+                       'rule:admin_only',
+                       description='Rule for getting advertised routes'),
+]
+
+
+def list_rules():
+    return rules
+
