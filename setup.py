ba789a9b2c0cfba8638a58977f8f31e4265e9285
Hongbin Lu <hongbin.lu@huawei.com>
2018-07-19 18:41:48 +0000

    [DNM] Test neutron change
    Depends-On: I21bf8a752813802822fd9966dda6ab3b6c4abfdc
    Change-Id: Id372082becb86f5c3ebefe3309e537bc34c4207b

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# test
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
