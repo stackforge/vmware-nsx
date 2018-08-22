c64468d3c1ef1da734687fe8d80b83ca48a5957d
Adit Sarfaty <asarfaty@vmware.com>
2018-08-22 15:12:11 +0300

    NSX|V3: Fix external LB member create
    fixed_ip & router_id parameters were swapped, causing external network member
    creation to fail.
    
    Change-Id: I57e5bfa91e49ad22425e91ad61b9bff2563a81d0

--- vmware_nsx/services/lbaas/nsx_v3/implementation/member_mgr.py
+++ vmware_nsx/services/lbaas/nsx_v3/implementation/member_mgr.py
@@ -38,6 +38,7 @@ class EdgeMemberManagerFromDict(base_mgr.Nsxv3LoadbalancerBaseManager):
 
     @log_helpers.log_method_call
     def _get_info_from_fip(self, context, fip):
+        LOG.error("DEBUG ADIT _get_info_from_fip %s", fip)
         filters = {'floating_ip_address': [fip]}
         floating_ips = self.core_plugin.get_floatingips(context,
                                                         filters=filters)
@@ -128,8 +129,13 @@ class EdgeMemberManagerFromDict(base_mgr.Nsxv3LoadbalancerBaseManager):
         network = lb_utils.get_network_from_subnet(
             context, self.core_plugin, member['subnet_id'])
         if network.get('router:external'):
-            router_id, fixed_ip = self._get_info_from_fip(
+            fixed_ip, router_id = self._get_info_from_fip(
                 context, member['address'])
+            if not router_id:
+                completor(success=False)
+                msg = (_('Floating ip %(fip)s has no router') % {
+                    'fip': member['address']})
+                raise n_exc.BadRequest(resource='lbaas-vip', msg=msg)
         else:
             router_id = lb_utils.get_router_from_network(
                 context, self.core_plugin, member['subnet_id'])
