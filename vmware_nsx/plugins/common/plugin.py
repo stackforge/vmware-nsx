f910250adf94e4334db935eec2d8e25794e68005
Michal Kelner Mishali <mkelnermishal@vmware.com>
2018-09-13 12:02:38 +0300

    NSX|V3: Fix bug in checking lbaas port dev-owner
    Fix bug in checking for using allowed-address-pairs
    on a LBAAS port
    
    Change-Id: Ie4f80c3bea7c9e4779b979e41cbb8530ce91803c
    Signed-off-by: Michal Kelner Mishali <mkelnermishal@vmware.com>

--- vmware_nsx/plugins/common/plugin.py
+++ vmware_nsx/plugins/common/plugin.py
@@ -481,8 +481,9 @@ class NsxPluginBase(db_base_plugin_v2.NeutronDbPluginV2,
     def _assert_on_device_owner_change(self, port_data, orig_dev_own):
         """Prevent illegal device owner modifications
         """
-        if orig_dev_own == constants.DEVICE_OWNER_LOADBALANCERV2:
-            if port_data['allowed_address_pairs']:
+        if (orig_dev_own and orig_dev_own ==
+                constants.DEVICE_OWNER_LOADBALANCERV2):
+            if port_data and "allowed_address_pairs" in port_data:
                 msg = _('Loadbalancer port can not be updated '
                         'with address pairs')
                 raise n_exc.InvalidInput(error_message=msg)
