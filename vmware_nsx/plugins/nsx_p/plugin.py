3ee1936787c440682a77c69ddc3705696bacdb21
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2018-10-25 21:18:46 -0700

    NSX|P: Handle port security in port creation
    Port update will be handled in follow-up
    
    Change-Id: I4e8a64f5730f359f5b5b4a6c93da13e123ac6321

--- vmware_nsx/plugins/nsx_p/plugin.py
+++ vmware_nsx/plugins/nsx_p/plugin.py
@@ -39,6 +39,7 @@ from neutron.db import portsecurity_db
 from neutron.db import securitygroups_db
 from neutron.db import vlantransparent_db
 from neutron.extensions import providernet
+from neutron.extensions import securitygroup as ext_sg
 from neutron.quota import resource_registry
 from neutron_lib.api.definitions import allowedaddresspairs as addr_apidef
 from neutron_lib.api.definitions import external_net
@@ -253,7 +254,7 @@ class NsxPolicyPlugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
                                  webob.exc.HTTPBadRequest,
                                  })
 
-    def _create_network_at_the_backend(self, context, net_data):
+    def _create_network_on_backend(self, context, net_data):
         # TODO(annak): provider network
         net_data['id'] = net_data.get('id') or uuidutils.generate_uuid()
 
@@ -307,7 +308,7 @@ class NsxPolicyPlugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
         # Create the backend NSX network
         if not is_external_net:
             try:
-                self._create_network_at_the_backend(context, created_net)
+                self._create_network_on_backend(context, created_net)
             except Exception as e:
                 LOG.exception("Failed to create NSX network network: %s", e)
                 with excutils.save_and_reraise_exception():
@@ -420,7 +421,19 @@ class NsxPolicyPlugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
 
         return address_bindings
 
-    def _create_port_at_the_backend(self, context, port_data):
+    def _build_port_tags(self, port_data):
+        sec_groups = port_data.get(ext_sg.SECURITYGROUPS, [])
+        sec_groups += port_data.get(provider_sg.PROVIDER_SECURITYGROUPS, [])
+
+        tags = []
+        for sg in sec_groups:
+            tags = nsxlib_utils.add_v3_tag(tags,
+                                           NSX_P_SECURITY_GROUP_TAG,
+                                           sg)
+
+        return tags
+
+    def _create_port_on_backend(self, context, port_data):
         # TODO(annak): admin_state not supported by policy
         # TODO(annak): handle exclude list
         # TODO(annak): switching profiles when supported
@@ -434,12 +447,15 @@ class NsxPolicyPlugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
         if device_owner and device_owner != l3_db.DEVICE_OWNER_ROUTER_INTF:
             vif_id = port_data['id']
 
+        tags = self._build_port_tags(port_data)
+
         self.nsxpolicy.segment_port.create_or_overwrite(
             name,
             port_data['network_id'],
             port_id=port_data['id'],
             description=port_data.get('description'),
             address_bindings=address_bindings,
+            tags=tags,
             vif_id=vif_id)
 
     def _cleanup_port(self, context, port_id, lport_id):
@@ -469,11 +485,19 @@ class NsxPolicyPlugin(agentschedulers_db.AZDhcpAgentSchedulerDbMixin,
             neutron_db = self.base_create_port(context, port)
             port["port"].update(neutron_db)
 
-            self._create_port_address_pairs(context, port_data)
+            # TODO(annak): check if ENS is relevant
+            (is_psec_on, has_ip, sgids, psgids) = (
+                self._create_port_preprocess_security(context, port,
+                                                      port_data, neutron_db,
+                                                      False))
+
+            self._process_port_create_security_group(context, port_data, sgids)
+            self._process_port_create_provider_security_group(
+                context, port_data, psgids)
 
         if not is_external_net:
             try:
-                self._create_port_at_the_backend(context, port_data)
+                self._create_port_on_backend(context, port_data)
             except Exception as e:
                 with excutils.save_and_reraise_exception():
                     LOG.error('Failed to create port %(id)s on NSX '
