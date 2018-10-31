06332a5a61777ed342ed538ac114f91c13e2bfb1
Adit Sarfaty <asarfaty@vmware.com>
2018-10-31 09:57:40 +0200

    TVD use loadbalancer project when creating LB objects
    In case the project plugin of the loadbalancer does not match the one
    of the new object, prefer the loadbalancer plugin (or else things will
    fail later)
    
    Change-Id: If7a43d46223ccc966665ce2f6bbc6c9fec9cc5f1

--- vmware_nsx/services/lbaas/nsx/lb_driver_v2.py
+++ vmware_nsx/services/lbaas/nsx/lb_driver_v2.py
@@ -49,7 +49,7 @@ class EdgeLoadBalancerManager(base_mgr.LoadbalancerBaseManager):
         if lb_p.plugin_type() != subnet_p.plugin_type():
             self.lbv2_driver.load_balancer.failed_completion(context, lb)
             msg = (_('Subnet must belong to the plugin %s, as the '
-                     'loadbalancer.') % lb_p.plugin_type())
+                     'loadbalancer') % lb_p.plugin_type())
             raise n_exc.BadRequest(resource='edge-lbaas', msg=msg)
         return lb_p.lbv2_driver.loadbalancer.create(context, lb)
 
@@ -87,17 +87,23 @@ class EdgeLoadBalancerManager(base_mgr.LoadbalancerBaseManager):
 
 class EdgeListenerManager(base_mgr.LoadbalancerBaseManager):
 
+    def _get_tenant_id(self, listener):
+        """Get the tenant of the loadbalancer itself if possible"""
+        if listener.loadbalancer:
+            return listener.loadbalancer.tenant_id
+        return listener.tenant_id
+
     @log_helpers.log_method_call
     def create(self, context, listener, certificate=None):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      listener.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(listener))
         return p.lbv2_driver.listener.create(context, listener,
                                              certificate=certificate)
 
     @log_helpers.log_method_call
     def update(self, context, old_listener, new_listener, certificate=None):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      new_listener.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(new_listener))
         return p.lbv2_driver.listener.update(context,
                                              old_listener,
                                              new_listener,
@@ -105,111 +111,142 @@ class EdgeListenerManager(base_mgr.LoadbalancerBaseManager):
 
     @log_helpers.log_method_call
     def delete(self, context, listener):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      listener.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(listener))
         return p.lbv2_driver.listener.delete(context, listener)
 
 
 class EdgePoolManager(base_mgr.LoadbalancerBaseManager):
 
+    def _get_tenant_id(self, pool):
+        """Get the tenant of the loadbalancer itself if possible"""
+        if pool.loadbalancer:
+            return pool.loadbalancer.tenant_id
+        return pool.tenant_id
+
     @log_helpers.log_method_call
     def create(self, context, pool):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      pool.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(pool))
         return p.lbv2_driver.pool.create(context, pool)
 
     @log_helpers.log_method_call
     def update(self, context, old_pool, new_pool):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      new_pool.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(new_pool))
         return p.lbv2_driver.pool.update(context, old_pool, new_pool)
 
     @log_helpers.log_method_call
     def delete(self, context, pool):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      pool.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(pool))
         return p.lbv2_driver.pool.delete(context, pool)
 
 
 class EdgeMemberManager(base_mgr.LoadbalancerBaseManager):
 
+    def _get_tenant_id(self, member):
+        """Get the tenant of the loadbalancer itself if possible"""
+        if member.pool and member.pool.loadbalancer:
+            return member.pool.loadbalancer.tenant_id
+        return member.tenant_id
+
     @log_helpers.log_method_call
     def create(self, context, member):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      member.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(member))
         return p.lbv2_driver.member.create(context, member)
 
     @log_helpers.log_method_call
     def update(self, context, old_member, new_member):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      new_member.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(new_member))
         return p.lbv2_driver.member.update(context, old_member, new_member)
 
     @log_helpers.log_method_call
     def delete(self, context, member):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      member.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(member))
         return p.lbv2_driver.member.delete(context, member)
 
 
 class EdgeHealthMonitorManager(base_mgr.LoadbalancerBaseManager):
 
+    def _get_tenant_id(self, hm):
+        """Get the tenant of the loadbalancer itself if possible"""
+        if hm.pool and hm.pool.loadbalancer:
+            return hm.pool.loadbalancer.tenant_id
+        return hm.tenant_id
+
     @log_helpers.log_method_call
     def create(self, context, hm):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      hm.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(hm))
         return p.lbv2_driver.healthmonitor.create(context, hm)
 
     @log_helpers.log_method_call
     def update(self, context, old_hm, new_hm):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      new_hm.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(new_hm))
         return p.lbv2_driver.healthmonitor.update(context, old_hm, new_hm)
 
     @log_helpers.log_method_call
     def delete(self, context, hm):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      hm.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(hm))
         return p.lbv2_driver.healthmonitor.delete(context, hm)
 
 
 class EdgeL7PolicyManager(base_mgr.LoadbalancerBaseManager):
 
+    def _get_tenant_id(self, policy):
+        """Get the tenant of the loadbalancer itself if possible"""
+        if policy.listener and policy.listener.loadbalancer:
+            return policy.listener.loadbalancer.tenant_id
+        return policy.tenant_id
+
     @log_helpers.log_method_call
     def create(self, context, policy):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      policy.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(policy))
         return p.lbv2_driver.l7policy.create(context, policy)
 
     @log_helpers.log_method_call
     def update(self, context, old_policy, new_policy):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      new_policy.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(new_policy))
         return p.lbv2_driver.l7policy.update(context, old_policy, new_policy)
 
     @log_helpers.log_method_call
     def delete(self, context, policy):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      policy.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(policy))
         return p.lbv2_driver.l7policy.delete(context, policy)
 
 
 class EdgeL7RuleManager(base_mgr.LoadbalancerBaseManager):
 
+    def _get_tenant_id(self, rule):
+        """Get the tenant of the loadbalancer itself if possible"""
+        if (rule.policy and rule.policy.listener and
+            rule.policy.listener.loadbalancer):
+            return rule.policy.listener.loadbalancer.tenant_id
+        return rule.tenant_id
+
     @log_helpers.log_method_call
     def create(self, context, rule):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      rule.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(rule))
         return p.lbv2_driver.l7rule.create(context, rule)
 
     @log_helpers.log_method_call
     def update(self, context, old_rule, new_rule):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      new_rule.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(new_rule))
         return p.lbv2_driver.l7rule.update(context, old_rule, new_rule)
 
     @log_helpers.log_method_call
     def delete(self, context, rule):
-        p = self.core_plugin._get_plugin_from_project(context,
-                                                      rule.tenant_id)
+        p = self.core_plugin._get_plugin_from_project(
+            context, self._get_tenant_id(rule))
         return p.lbv2_driver.l7rule.delete(context, rule)
