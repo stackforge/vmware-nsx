c5eaf5b539a8f5b05e2d3bdc4302dc7a89145e15
Michal Kelner Mishali <mkelnermishal@vmware.com>
2018-12-12 14:52:43 +0200

    NSX|T: Optional distinct edge cluster uuid for T1 router
    Allowing the user to define a different edge cluster UUID than
    the one defined by default, allowing the ability to define another
    cluster for the T1 logical router.
    
    Change-Id: I7976a90b2134a53c3ff80e5f0785f999c05137d3
    Signed-off-by: Michal Kelner Mishali <mkelnermishal@vmware.com>

--- vmware_nsx/tests/unit/nsx_v3/test_plugin.py
+++ vmware_nsx/tests/unit/nsx_v3/test_plugin.py
@@ -72,6 +72,7 @@ NSX_DHCP_PROFILE_ID = 'default dhcp profile'
 NSX_METADATA_PROXY_ID = 'default metadata proxy'
 NSX_SWITCH_PROFILE = 'dummy switch profile'
 NSX_DHCP_RELAY_SRV = 'dhcp relay srv'
+EDGE_CLUSTER = '15f63b04-3cd8-4dbd-9f51-0b0dddc82a84'
 
 
 def _mock_create_firewall_rules(*args):
@@ -143,6 +144,11 @@ def _mock_nsx_backend_calls():
         return_value=NSX_DHCP_PROFILE_ID).start()
 
     mock.patch(
+        "vmware_nsxlib.v3.core_resources.NsxLibEdgeCluster."
+        "get_id_by_name_or_id",
+        return_value=EDGE_CLUSTER).start()
+
+    mock.patch(
         "vmware_nsxlib.v3.core_resources.NsxLibDhcpRelayService."
         "get_id_by_name_or_id",
         return_value=NSX_DHCP_RELAY_SRV).start()
@@ -207,10 +213,10 @@ class NsxV3PluginTestCaseMixin(test_plugin.NeutronDbPluginV2TestCase,
             nsx_plugin.NsxV3Plugin, '_ensure_global_sg_placeholder')
         mock_ensure_global_sg_placeholder.start()
 
-        mock_get_edge_cluster = mock.patch.object(
-            nsx_plugin.NsxV3Plugin, '_get_edge_cluster',
-            return_value=uuidutils.generate_uuid())
-        mock_get_edge_cluster.start()
+        # mock_get_edge_cluster = mock.patch.object(
+        #     nsx_plugin.NsxV3Plugin, '_get_edge_cluster',
+        #     return_value=uuidutils.generate_uuid())
+        # mock_get_edge_cluster.start()
         mock.patch(
             'neutron_lib.rpc.Connection.consume_in_threads',
             return_value=[]).start()
@@ -3004,6 +3010,25 @@ class TestL3NatTestCase(L3NatTest,
     def test_route_update_illegal(self):
         self._test_route_update_illegal('0.0.0.0/0')
         self._test_route_update_illegal('0.0.0.0/16')
+
+    def test_update_router_distinct_edge_cluster(self):
+        edge_cluster = EDGE_CLUSTER
+        cfg.CONF.set_override('edge_cluster', edge_cluster, 'nsx_v3')
+        with self.address_scope(name='as1') as addr_scope, \
+                self._create_l3_ext_network() as ext_net:
+            ext_subnet = self._prepare_external_subnet_on_address_scope(
+                ext_net, addr_scope)
+
+            # create a router with this gateway
+            with self.router() as r, \
+                    self._mock_add_remove_service_router() as change_sr:
+                router_id = r['router']['id']
+                self.plugin.init_availability_zones()
+                for az in self.plugin.get_azs_list():
+                    az.translate_configured_names_to_uuids(self.plugin.nsxlib)
+                self._add_external_gateway_to_router(
+                    router_id, ext_subnet['network_id'])
+                change_sr.assert_called_once_with(mock.ANY, edge_cluster)
 
 
 class ExtGwModeTestCase(test_ext_gw_mode.ExtGwModeIntTestCase,
