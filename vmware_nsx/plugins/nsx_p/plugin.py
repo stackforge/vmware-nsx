992b2e3b488d9e91c15580dfa3058fa6ade5e5ea
Kobi Samoray <ksamoray@vmware.com>
2018-12-24 14:19:43 +0200

    NSXP: LBaaS/Octavia support
    Change-Id: I2bd7b01f921243d65f68ec328173e949607e7842

--- vmware_nsx/plugins/nsx_p/plugin.py
+++ vmware_nsx/plugins/nsx_p/plugin.py
@@ -60,6 +60,7 @@ from vmware_nsx.extensions import securitygrouplogging as sg_logging
 from vmware_nsx.plugins.common_v3 import plugin as nsx_plugin_common
 from vmware_nsx.plugins.nsx_p import availability_zones as nsxp_az
 from vmware_nsx.plugins.nsx_v3 import utils as v3_utils
+from vmware_nsx.services.lbaas.nsx_p.v2 import lb_driver_v2
 
 from vmware_nsxlib.v3 import exceptions as nsx_lib_exc
 from vmware_nsxlib.v3 import nsx_constants as nsxlib_consts
@@ -166,6 +167,7 @@ class NsxPolicyPlugin(nsx_plugin_common.NsxPluginV3Base):
         self._init_default_config()
         self._prepare_default_rules()
         self._init_segment_profiles()
+        self.lbv2_driver = self._init_lbv2_driver()
 
         for az in self.get_azs_list():
             az.translate_configured_names_to_uuids(self.nsxpolicy)
@@ -259,6 +261,13 @@ class NsxPolicyPlugin(nsx_plugin_common.NsxPluginV3Base):
     @staticmethod
     def plugin_type():
         return projectpluginmap.NsxPlugins.NSX_P
+
+    def _init_lbv2_driver(self):
+        # Get LBaaSv2 driver during plugin initialization. If the platform
+        # has a version that doesn't support native loadbalancing, the driver
+        # will return a NotImplementedManager class.
+        LOG.debug("Initializing LBaaSv2.0 nsxp driver")
+        return lb_driver_v2.EdgeLoadbalancerDriverV2()
 
     @staticmethod
     def is_tvd_plugin():
@@ -1858,3 +1867,6 @@ class NsxPolicyPlugin(nsx_plugin_common.NsxPluginV3Base):
 
     def _is_vlan_router_interface_supported(self):
         return True
+
+    def service_router_has_services(self, context, router_id):
+        pass
