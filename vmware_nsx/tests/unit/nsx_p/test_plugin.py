de04e633a61ea280ee356cab746d15cbc7d7987c
Adit Sarfaty <asarfaty@vmware.com>
2019-01-01 17:44:58 +0200

    NSX|P metadata proxy support
    Using the passthrough api with the same code as the V3 plugin
    Also updatinh the availability zones initialization to support this
    
    Change-Id: I46c50a2115dc092f3abd309546cb0e2d9750d711

--- vmware_nsx/tests/unit/nsx_p/test_plugin.py
+++ vmware_nsx/tests/unit/nsx_p/test_plugin.py
@@ -58,6 +58,7 @@ NSX_OVERLAY_TZ_NAME = 'OVERLAY_TZ'
 NSX_VLAN_TZ_NAME = 'VLAN_TZ'
 DEFAULT_TIER0_ROUTER_UUID = "efad0078-9204-4b46-a2d8-d4dd31ed448f"
 NSX_DHCP_PROFILE_ID = 'DHCP_PROFILE'
+NSX_MD_PROXY_ID = 'MD_PROXY'
 LOGICAL_SWITCH_ID = '00000000-1111-2222-3333-444444444444'
 
 
@@ -145,11 +146,15 @@ class NsxPPluginTestCaseMixin(
             "vmware_nsxlib.v3.resources.LogicalDhcpServer.create_binding",
             side_effect=_return_id_key).start()
 
+        mock.patch("vmware_nsxlib.v3.NsxLib."
+                   "get_id_by_resource_and_tag").start()
+
     def setup_conf_overrides(self):
         cfg.CONF.set_override('default_overlay_tz', NSX_OVERLAY_TZ_NAME,
                               'nsx_p')
         cfg.CONF.set_override('default_vlan_tz', NSX_VLAN_TZ_NAME, 'nsx_p')
         cfg.CONF.set_override('dhcp_profile', NSX_DHCP_PROFILE_ID, 'nsx_p')
+        cfg.CONF.set_override('metadata_proxy', NSX_MD_PROXY_ID, 'nsx_p')
 
     def _create_network(self, fmt, name, admin_state_up,
                         arg_list=None, providernet_args=None,
